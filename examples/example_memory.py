a41d580bf96b39e18cc11959b687a680ac3cf9d1
medivh-xp <medivh-x@outlook.com>
2023-07-07 10:33:48 +0800

    add testcase for memory leak case

--- examples/example_memory.py
+++ examples/example_memory.py
@@ -8,6 +8,10 @@ import torchair as tng
 from torchair.ge_concrete_graph import ge_apis as ge
 from torchair.configs.compiler_config import CompilerConfig
 
+import logging
+from torchair.core.utils import logger
+logger.setLevel(logging.DEBUG)
+
 config = CompilerConfig()
 config.aoe_config.aoe_mode = "1"
 config.debug.graph_dump.type = "pbtxt"
@@ -15,22 +19,15 @@ config.debug.graph_dump.type = "pbtxt"
 npu_backend = tng.get_npu_backend(compiler_config=config)
 
 
-class Model2(torch.nn.Module):
+class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
     def forward(self, x, y):
         return torch.add(x, y)
 
-class Model(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
 
-    def forward(self, x, y):
-        return x + y
-
-
-model = Model2()
+model = Model()
 model = torch.compile(model, backend=npu_backend, dynamic=True)
 x = torch.randn(512, 1024, 1024)
 y = torch.randn(512, 1024, 1024)
