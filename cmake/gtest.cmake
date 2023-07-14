32d9f91d776ad5b2438f0b9be123f54cdefcc7da
medivh-xp <medivh-x@outlook.com>
2023-07-14 17:51:25 +0800

    change PKG server address

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,8 +1,8 @@
 include(FetchContent)
-if(TF_PKG_SERVER)
+if(CI_PKG_SERVER)
   FetchContent_Declare(
           googletest
-          URL ${TF_PKG_SERVER}/libs/ge_gtest/release-1.8.1.tar.gz
+          URL ${CI_PKG_SERVER}/libs/ge_gtest/release-1.8.1.tar.gz
           URL_HASH MD5=2e6fbeb6a91310a16efe181886c59596
   )
 else()
