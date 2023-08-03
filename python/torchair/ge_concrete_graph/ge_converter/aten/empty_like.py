044e718b56452222a7a2b4e72a5b4bae14818ab0
王姜奔 <wangjiangben@huawei.com>
2023-08-03 03:32:07 +0000

    !25 新增empty_like适配 Merge pull request !25 from 王姜奔/new2

--- python/torchair/ge_concrete_graph/ge_converter/aten/empty_like.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/empty_like.py
@@ -18,10 +18,18 @@ import torch
 from torch import Generator, contiguous_format, inf, strided
 from torch.types import Device, Number, SymInt, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
-from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter
+from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter, declare_supported
 from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
+from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
+    Support
+from torchair.ge_concrete_graph.utils import dtype_promote
 
 
+@declare_supported([
+    Support(F32(2, 2)),
+    Support(F16(2, 2)),
+    Support(I32(2, 2)),
+])
 @register_fx_node_ge_converter(torch.ops.aten.empty_like.default)
 def conveter_aten_empty_like_default(
     self: Tensor,
@@ -34,7 +42,17 @@ def conveter_aten_empty_like_default(
     meta_outputs: Union[TensorSpec, List[TensorSpec]] = None
 ):
     """NB: aten::empty_like(Tensor self, *, ScalarType? dtype=None, Layout? layout=None, Device? device=None, bool? pin_memory=None, MemoryFormat? memory_format=None) -> Tensor"""
-    raise NotImplementedError("torch.ops.aten.empty_like.default ge_converter is not implemented!")
+    
+    if dtype is None:
+        dtype = self.dtype
+
+    if layout is not None and (layout != torch.strided):
+        raise NotImplementedError("torch.ops.aten.empty_like.default is only supported on dense tensor now.")
+    
+    if memory_format is not None and (memory_format != torch.contiguous_format):
+        raise NotImplementedError("torch.ops.aten.empty_like.default is only supported contiguous_format now.")
+    
+    return ge.Empty(ge.Shape(self), dtype=dtype)
 
 
 @register_fx_node_ge_converter(torch.ops.aten.empty_like.out)
