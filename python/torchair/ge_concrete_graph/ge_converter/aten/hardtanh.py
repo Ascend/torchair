2e915f56d2af0125511b4933bb427968d4567fe2
yuhaiyan <yuhaiyan8@huawei.com>
2023-08-16 02:04:44 +0000

    !42 【Pytorch】aten.hardtanh.default/aten.sigmoid.default/aten.sigmoid_backward.defautl/aten.stack.default 适配 Merge pull request !42 from yuhaiyan/master

--- python/torchair/ge_concrete_graph/ge_converter/aten/hardtanh.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/hardtanh.py
@@ -18,10 +18,19 @@ import torch
 from torch import Generator, contiguous_format, inf, strided
 from torch.types import Device, Number, SymInt, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
-from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter
+from torchair.ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
 from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
+from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
+    Support
 
 
+@declare_supported(
+    [
+        Support(F32(96, 384, 14, 14), 0.0, 6.0),
+        Support(F16(96, 384, 14, 14), 0.0, 6.0),
+        Support(F32(2, 1280, 7, 7), 0.0, 6.0),
+    ]
+)
 @register_fx_node_ge_converter(torch.ops.aten.hardtanh.default)
 def conveter_aten_hardtanh_default(
     self: Tensor,
@@ -30,7 +39,7 @@ def conveter_aten_hardtanh_default(
     meta_outputs: TensorSpec = None,
 ):
     """NB: aten::hardtanh(Tensor self, Scalar min_val=-1, Scalar max_val=1) -> Tensor"""
-    raise NotImplementedError("torch.ops.aten.hardtanh.default ge_converter is not implemented!")
+    return ge.ClipByValue(self, min_val, max_val)
 
 
 @register_fx_node_ge_converter(torch.ops.aten.hardtanh.out)
