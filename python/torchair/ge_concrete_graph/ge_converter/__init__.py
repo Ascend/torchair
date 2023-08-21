bbc5cad5a393266938fcc12b5f56f9a32282a7df
wbigat <wbigat@163.com>
2023-08-21 19:44:13 +0800

    Follow the community to remove nvprims converter.

--- python/torchair/ge_concrete_graph/ge_converter/__init__.py
+++ python/torchair/ge_concrete_graph/ge_converter/__init__.py
@@ -1,5 +1,4 @@
 from torchair.ge_concrete_graph.ge_converter.aten import *
-from torchair.ge_concrete_graph.ge_converter.nvprims import *
 from torchair.ge_concrete_graph.ge_converter.prim import *
 from torchair.ge_concrete_graph.ge_converter.prims import *
 from torchair.ge_concrete_graph.ge_converter.quantized import *
