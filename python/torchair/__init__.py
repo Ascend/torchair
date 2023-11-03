06bd87a759d252e7b2f5cc49cea018c30d885da0
王强 <wangqiang160@hisilicon.com>
2023-11-03 01:40:05 +0000

    !169 add requirements version check Merge pull request !169 from 王强/master

--- python/torchair/__init__.py
+++ python/torchair/__init__.py
@@ -1,7 +1,16 @@
 import torch
+import pkg_resources
+
 from torchair.npu_fx_compiler import get_compiler
 from torchair.npu_fx_compiler import get_npu_backend
 from torchair.dynamo_export import dynamo_export
 from torchair.core.backend import stupid_repeat
 from torchair.configs.compiler_config import CompilerConfig
 from torchair.ge_concrete_graph import ge_converter
+
+
+# Dependency library version verification
+protobuf_version = pkg_resources.get_distribution("protobuf").version
+
+assert pkg_resources.parse_version(protobuf_version) >= pkg_resources.parse_version("3.13")
+assert pkg_resources.parse_version(protobuf_version) < pkg_resources.parse_version("4")
