89466bc30f98489397977b3df27cd1f2e442f665
鲁博洋 <luboyang@huawei.com>
2023-11-18 10:09:42 +0000

    !190 update convert torch.ops.aten.split_with_sizes.default Merge pull request !190 from 鲁博洋/luboyang-master-patch-57719

--- python/torchair/ge_concrete_graph/ge_converter/aten/split_with_sizes.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/split_with_sizes.py
@@ -18,10 +18,16 @@ import torch
 from torch import Generator, contiguous_format, inf, strided
 from torch.types import Device, Number, SymInt, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
-from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter
-from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
+from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter, declare_supported
+from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec, DataType
+from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
+    Support
+from torchair.ge_concrete_graph.utils import dtype_promote
 
 
+@declare_supported([
+    Support(F32(4, 1, 1664), [1408, 128, 128], 2)
+])
 @register_fx_node_ge_converter(torch.ops.aten.split_with_sizes.default)
 def conveter_aten_split_with_sizes_default(
     self: Tensor,
@@ -30,4 +36,9 @@ def conveter_aten_split_with_sizes_default(
     meta_outputs: List[TensorSpec] = None,
 ):
     """NB: aten::split_with_sizes(Tensor(a -> *) self, SymInt[] split_sizes, int dim=0) -> Tensor(a)[]"""
-    raise NotImplementedError("torch.ops.aten.split_with_sizes.default ge_converter is not implemented!")
+    if isinstance(split_sizes, list):
+        return ge.SplitV(self, split_sizes, dim, num_split=len(meta_outputs))
+    elif isinstance(split_sizes, Tensor):
+        return ge.SplitV(self, split_sizes, dim, num_split=len(meta_outputs))
+    else:
+        raise NotImplementedError("torch.ops.aten.split_with_sizes.default is not support this type input!")
