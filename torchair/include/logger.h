f9bfef2044a7b6e634ed52ce51bbf7b915213257
王强 <wangqiang160@hisilicon.com>
2023-12-12 12:23:22 +0000

    !245 add log level Merge pull request !245 from 王强/master

--- torchair/include/logger.h
+++ torchair/include/logger.h
@@ -7,7 +7,9 @@
 namespace tng {
 class Logger : public std::basic_ostringstream<char> {
  public:
-  Logger(const char *f, int line) { *this << f << ":" << line << " "; }
+  Logger(const char *f, int line, const char *log_level) {
+    *this << "[" << log_level << "] " << f << ":" << line << " ";
+  }
   ~Logger() override { std::cerr << str() << std::endl; }
 };
 
@@ -17,6 +19,6 @@ const static int32_t kLogLevel =
   std::getenv("TNG_LOG_LEVEL") ? atoi(std::getenv("TNG_LOG_LEVEL")) : static_cast<int32_t>(tng::LogLevel::ERROR);
 
 #define TNG_LOG(L) \
-  if (static_cast<int32_t>(tng::LogLevel::L) >= kLogLevel) tng::Logger(__FILE__, __LINE__)
+  if (static_cast<int32_t>(tng::LogLevel::L) >= kLogLevel) tng::Logger(__FILE__, __LINE__, (#L))
 
 #endif  // TORCH_AIR_TORCH_AIR_CONCRETE_GRAPH_LOGGER_H_
