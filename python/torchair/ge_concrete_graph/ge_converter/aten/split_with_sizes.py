2f194273388a6229fc84804d123f053cfbcbf373
鲁博洋 <luboyang@huawei.com>
2023-12-14 11:15:04 +0000

    !247 fix split with size and empty Merge pull request !247 from 鲁博洋/convert-bugfix

--- python/torchair/ge_concrete_graph/ge_converter/aten/split_with_sizes.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/split_with_sizes.py
@@ -26,7 +26,8 @@ from torchair.ge_concrete_graph.utils import dtype_promote
 
 
 @declare_supported([
-    Support(F32(4, 1, 1664), [1408, 128, 128], 2)
+    Support(F32(4, 1, 1664), [1408, 128, 128], 2),
+    Support(U8(4, 1, 1664), [1408, 128, 128], 2)
 ])
 @register_fx_node_ge_converter(torch.ops.aten.split_with_sizes.default)
 def conveter_aten_split_with_sizes_default(
@@ -36,4 +37,9 @@ def conveter_aten_split_with_sizes_default(
     meta_outputs: List[TensorSpec] = None,
 ):
     """NB: aten::split_with_sizes(Tensor(a -> *) self, SymInt[] split_sizes, int dim=0) -> Tensor(a)[]"""
+    split_sizes = dtype_promote(split_sizes, target_dtype=DataType.DT_INT64)
+    if dim > 2147483647:
+        dim = dtype_promote(dim, target_dtype=DataType.DT_INT64)
+    else:
+        dim = dtype_promote(dim, target_dtype=DataType.DT_INT32)
     return ge.SplitV(self, split_sizes, dim, num_split=len(meta_outputs))
