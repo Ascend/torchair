ca6a75e0d2b2a8ccb47ad7189c4bb434a8367282
鲁博洋 <luboyang@huawei.com>
2023-12-14 11:15:32 +0000

    !250 alias and unbind convert add Merge pull request !250 from 鲁博洋/qwen-convert

--- python/torchair/ge_concrete_graph/ge_converter/aten/unbind.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/unbind.py
@@ -18,14 +18,21 @@ import torch
 from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
-from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter
-from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
+from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter, declare_supported
+from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec, DataType
+from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
+    Support
+from torchair.ge_concrete_graph.utils import dtype_promote
 
 
+@declare_supported([
+    Support(F32(1, 1, 40, 2, 64), 3)
+])
 @register_fx_node_ge_converter(torch.ops.aten.unbind.int)
 def conveter_aten_unbind_int(self: Tensor, dim: int = 0, meta_outputs: List[TensorSpec] = None):
     """NB: aten::unbind.int(Tensor(a -> *) self, int dim=0) -> Tensor(a)[]"""
-    raise NotImplementedError("torch.ops.aten.unbind.int ge_converter is not implemented!")
+    num = len(meta_outputs)
+    return ge.Unpack(self, num=num, axis=dim)
 
 
 @register_fx_node_ge_converter(torch.ops.aten.unbind.Dimname)
