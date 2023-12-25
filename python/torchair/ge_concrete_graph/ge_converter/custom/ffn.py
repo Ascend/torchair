86258126c3ea203636b987d9340a9f04236d5752
zqh <zhouqinghong1@hisilicon.com>
2023-12-25 10:41:27 +0000

    !279 FFN回退torch接口新增量化伪量化参数 Merge pull request !279 from zqh/1225

--- python/torchair/ge_concrete_graph/ge_converter/custom/ffn.py
+++ python/torchair/ge_concrete_graph/ge_converter/custom/ffn.py
@@ -41,28 +41,13 @@ def conveter_npu_npu_ffn(
     expert_tokens: Optional[Union[List[int], Tensor]] = None,
     bias1: Optional[Tensor] = None,
     bias2: Optional[Tensor] = None,
-    scale: Optional[Tensor] = None,
-    offset: Optional[Tensor] = None,
-    deq_scale1: Optional[Tensor] = None,
-    deq_scale2: Optional[Tensor] = None,
-    antiquant_scale1: Optional[Tensor] = None,
-    antiquant_scale2: Optional[Tensor] = None,
-    antiquant_offset1: Optional[Tensor] = None,
-    antiquant_offset2: Optional[Tensor] = None,
     inner_precise: Optional[int] = 0
 ):
     """NB: npu::npu_ffn(Tensor x, Tensor weight1, Tensor weight2, str activation, int[]? expert_tokens=None,
-                        Tensor? bias1=None, Tensor? bias2=None, Tensor? scale=None, Tensor? offset=None,
-                        Tensor? deq_scale1=None, Tensor? deq_scale2=None, Tensor? antiquant_scale1=None,
-                        Tensor? antiquant_scale2=None, Tensor? antiquant_offset1=None,
-                        Tensor? antiquant_offset2=None, int? inner_precise=None) -> Tensor
+                        Tensor? bias1=None, Tensor? bias2=None, int? inner_precise=None) -> Tensor
     """
     if expert_tokens is not None and isinstance(expert_tokens, Tensor):
         raise NotImplementedError("FFN is not implemented while expert_tokens is Tensor!")
-    if scale is not None or offset is not None or deq_scale1 is not None or deq_scale2 is not None or \
-        antiquant_scale1 is not None or antiquant_scale2 is not None or antiquant_offset1 is not None or \
-        antiquant_offset2 is not None:
-        raise NotImplementedError("FFN dose not support quant case and antiquant case now!")
 
     return ge.FFN(x, weight1, weight2, expert_tokens=expert_tokens, bias1=bias1, bias2=bias2, scale=None,
         offset=None, deq_scale1=None, deq_scale2=None, antiquant_scale1=None, antiquant_scale2=None,
