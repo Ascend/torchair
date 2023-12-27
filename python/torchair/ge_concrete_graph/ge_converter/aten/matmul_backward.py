2b80e34933ac7cde1309b0cf55950f636a9c1067
鲁博洋 <luboyang@huawei.com>
2023-12-27 12:14:38 +0000

    !291 fix transpose dtype bug Merge pull request !291 from 鲁博洋/fix-transpose-bug

--- python/torchair/ge_concrete_graph/ge_converter/aten/matmul_backward.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/matmul_backward.py
@@ -19,15 +19,17 @@ from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
 from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter, declare_supported
-from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
+from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec, DataType
 from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
     Support
+from torchair.ge_concrete_graph.utils import dtype_promote
 
 
 @declare_supported(
     [
         Support(F32(4, 2, 128, 64), F32(4, 2, 128, 128), F32(4, 2, 128, 64), [True, True]),
         Support(F32(4, 2, 128, 128), F32(4, 2, 128, 64), F32(4, 2, 64, 128), [True, True]),
+        Support(I8(4, 2, 128, 128), I8(4, 2, 128, 64), I8(4, 2, 64, 128), [True, True]),
     ]
 )
 @register_fx_node_ge_converter(torch.ops.aten.matmul_backward.default)
@@ -46,7 +48,8 @@ def conveter_aten_matmul_backward_default(
 
     other_perm_list = [i for i in range(other.rank)]
     other_perm_list[-1], other_perm_list[-2] = other_perm_list[-2], other_perm_list[-1]
-
+    
+    self_perm_list, other_perm_list = dtype_promote(self_perm_list, other_perm_list, target_dtype=DataType.DT_INT64)
     grad_self = ge.BatchMatMul(grad, ge.Transpose(other, other_perm_list))
     grad_other = ge.BatchMatMul(ge.Transpose(self, self_perm_list), grad)
     return grad_self, grad_other
--- python/torchair/ge_concrete_graph/ge_converter/aten/transpose.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/transpose.py
@@ -18,10 +18,20 @@ import torch
 from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
-from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter
-from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
+from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter, declare_supported
+from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec, DataType
+from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
+    Support
+from torchair.ge_concrete_graph.utils import dtype_promote
 
 
+
+@declare_supported(
+    [
+        Support(F32(2, 3), 0, 1),
+        Support(I8(2, 3), 0, 1),
+    ]
+)
 @register_fx_node_ge_converter(torch.ops.aten.transpose.int)
 def conveter_aten_transpose_int(
     self: Tensor, dim0: int, dim1: int, meta_outputs: TensorSpec = None
@@ -29,6 +39,7 @@ def conveter_aten_transpose_int(
     """NB: aten::transpose.int(Tensor(a) self, int dim0, int dim1) -> Tensor(a)"""
     perm_list = [i for i in range(self.rank)]
     perm_list[dim0], perm_list[dim1] = perm_list[dim1], perm_list[dim0]
+    perm_list = dtype_promote(perm_list, target_dtype=DataType.DT_INT64)
     return ge.Transpose(self, perm_list)
 
 
