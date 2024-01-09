88b919adb79cbec74445eac106519e904307c4af
鲁博洋 <luboyang@huawei.com>
2024-01-09 12:23:53 +0000

    !323 fix reshape dtype bug Merge pull request !323 from 鲁博洋/fix-reshape-bug

--- python/torchair/ge_concrete_graph/ge_converter/aten/alias.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/alias.py
@@ -26,10 +26,12 @@ from torchair.ge_concrete_graph.utils import dtype_promote
 
 
 @declare_supported([
-    Support(F16(1, 1, 40, 128))
+    Support(F16(1, 1, 40, 128)),
+    Support(U8(1, 1, 40, 128))
 ])
 @register_fx_node_ge_converter(torch.ops.aten.alias.default)
 def conveter_aten_alias_default(self: Tensor, meta_outputs: TensorSpec = None):
     """NB: aten::alias(Tensor(a) self) -> Tensor(a)"""
     shape = ge.Shape(self)
+    shape = dtype_promote(shape, target_dtype=DataType.DT_INT64)
     return ge.Reshape(self, shape)
--- python/torchair/ge_concrete_graph/ge_converter/aten/argmax.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/argmax.py
@@ -19,14 +19,15 @@ from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
 from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter, declare_supported
-from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
-from torchair.ge_concrete_graph.utils import dtype_promote, DataType
+from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec, DataType
+from torchair.ge_concrete_graph.utils import dtype_promote
 from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
      Support
 
 
 @declare_supported([
      Support(F32(3, 2)),
+     Support(U8(3, 2)),
      Support(F32(3, 2), 1),
      Support(F32(3, 2), 1, keepdim=True),
 ])
@@ -39,7 +40,7 @@ def conveter_aten_argmax_default(
 ):
     """NB: aten::argmax(Tensor self, int? dim=None, bool keepdim=False) -> Tensor"""
     if not dim:
-        self_cp = ge.Reshape(self, (-1,))
+        self_cp = ge.Reshape(self, dtype_promote((-1,), target_dtype=DataType.DT_INT64))
         return ge.ArgMaxV2(self_cp, 0)
     if keepdim:
         index, _ = ge.ArgMaxWithValue(self, dimension=dim, keep_dims=keepdim)
