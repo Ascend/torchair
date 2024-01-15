95a6e26b64e5b3901d6ecffdf58f3df4544029ec
lilei zheng <zhenglilei@huawei.com>
2024-01-15 11:26:24 +0000

    !331 【AR20240113418065】【Conv2d】Conv2d算子在pytorch图模式下支持FZ格式的weight Merge pull request !331 from lilei zheng/weight_support_fz

--- python/torchair/experimental/inference.py
+++ python/torchair/experimental/inference.py
@@ -8,6 +8,17 @@ _TORCH_NPU_MODULE = None
 def _weight_format_cast(model: torch.nn.Module):
     def _cast_to_internal_format(module: torch.nn.Module, class_name):
         # Add weight format cast for other modules here
+        if issubclass(class_name, torch.nn.Conv2d):
+            if module.weight is None or module.weight.data is None:
+                return
+            if module.weight.data.is_cpu:
+                raise RuntimeError(f'Cpu weight is not supported.'
+                                   f'The format cast to FRACTAL_Z only supports npu tensor.'
+                                   f'You should call model to npu, before calling this API.')
+            if module.groups > 1 or module.weight.dtype != torch.float16:
+                return
+
+            module.weight.data = _TORCH_NPU_MODULE.npu_format_cast(module.weight.data, 4)  # ACL_FORMAT_FRACTAL_Z
 
         if issubclass(class_name, torch.nn.Linear):
             if module.weight.data.is_cpu:
