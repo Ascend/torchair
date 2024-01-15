b1f41d923139cc40093f06634e105449ca2f9ca2
chen-vvjob <chenweiwei22@huawei.com>
2024-01-15 01:15:11 +0000

    !295 修改PFA 算子入参 Merge pull request !295 from chen-vvjob/master

--- python/torchair/ge_concrete_graph/ge_converter/custom/flash_attention.py
+++ python/torchair/ge_concrete_graph/ge_converter/custom/flash_attention.py
@@ -45,6 +45,7 @@ def convert_npu_npu_prompt_flash_attention(
     *,
     padding_mask: Optional[Tensor] = None,
     atten_mask: Optional[Tensor] = None,
+    pse_shift: Optional[Tensor] = None,
     actual_seq_lengths: Optional[Union[List[int], Tensor]] = None,
     num_heads: int = 1,
     scale_value: float = 1.0,
@@ -57,13 +58,13 @@ def convert_npu_npu_prompt_flash_attention(
     meta_outputs: TensorSpec = None,
 ):
 
-    '''NB: npu::npu_prompt_flash_attention(Tensor query, Tensor key, Tensor value, *, Tensor? padding_mask=None, Tensor? atten_mask=None, int[]? actual_seq_lengths=None, int num_heads=1, float scale_value=1.0, int pre_tokens=2147473647, int next_tokens=0, str input_layout="BSH", int num_key_value_heads=0, int[]? actual_seq_lengths_kv=None, int sparse_mode=0) -> Tensor'''
+    '''NB: npu::npu_prompt_flash_attention(Tensor query, Tensor key, Tensor value, *, Tensor? padding_mask=None, Tensor? atten_mask=None, Tensor? pse_shift=None, int[]? actual_seq_lengths=None, int num_heads=1, float scale_value=1.0, int pre_tokens=2147473647, int next_tokens=0, str input_layout="BSH", int num_key_value_heads=0, int[]? actual_seq_lengths_kv=None, int sparse_mode=0) -> Tensor'''
     if actual_seq_lengths is not None and isinstance(actual_seq_lengths, Tensor):
         raise NotImplementedError("PromptFlashAttention is not implemented while actual_seq_lengths is Tensor!")
     if actual_seq_lengths_kv is not None and isinstance(actual_seq_lengths_kv, Tensor):
         raise NotImplementedError("PromptFlashAttention is not implemented while actual_seq_lengths_kv is Tensor!")
 
-    return ge.PromptFlashAttention(query, key, value, padding_mask=padding_mask, atten_mask=atten_mask,
+    return ge.PromptFlashAttention(query, key, value, pse_shift=pse_shift, atten_mask=atten_mask,
         actual_seq_lengths=actual_seq_lengths, actual_seq_lengths_kv=actual_seq_lengths_kv,
         deq_scale1=None, quant_scale1=None, deq_scale2=None, quant_scale2=None, quant_offset2=None,
         num_heads=num_heads, scale_value=scale_value,
