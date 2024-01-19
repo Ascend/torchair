b03fc152b3a0f68605e4e75ece624451f2440496
liuguoyue <liuguoyue@huawei.com>
2024-01-19 08:45:38 +0000

    !385 修改antiQuant参数命名风格 Merge pull request !385 from liuguoyue/antiQuant

--- python/torchair/ge_concrete_graph/ge_converter/custom/npu_anti_quant.py
+++ python/torchair/ge_concrete_graph/ge_converter/custom/npu_anti_quant.py
@@ -13,12 +13,12 @@ from torchair.ge_concrete_graph.supported_declaration import F32, I8, Support
 
 @declare_supported([
     Support(I8(3, 12, 20, 5), F32(1)),
-    Support(I8(3, 12, 17, 5), F32(1), dstDtype=torch.bfloat16),
-    Support(I8(3, 12, 17, 5), F32(1), offset=F32(1), dstDtype=torch.bfloat16, srcDtype=torch.int8),
+    Support(I8(3, 12, 17, 5), F32(1), dst_dtype=torch.bfloat16),
+    Support(I8(3, 12, 17, 5), F32(1), offset=F32(1), dst_dtype=torch.bfloat16, src_dtype=torch.int8),
     Support(I8(3, 12, 17, 5), F32(1), offset=F32(1)),
-    Support(I8(3, 12, 17, 5), F32(1), offset=F32(1), srcDtype=torch.int8),
-    Support(I8(3, 12, 17, 5), F32(1), dstDtype=torch.bfloat16, srcDtype=torch.int8),
-    Support(I8(3, 12, 17, 5), F32(1), dstDtype=torch.float16, srcDtype=torch.int8)    
+    Support(I8(3, 12, 17, 5), F32(1), offset=F32(1), src_dtype=torch.int8),
+    Support(I8(3, 12, 17, 5), F32(1), dst_dtype=torch.bfloat16, src_dtype=torch.int8),
+    Support(I8(3, 12, 17, 5), F32(1), dst_dtype=torch.float16, src_dtype=torch.int8)    
 ])
 @register_fx_node_ge_converter(torch.ops.npu.npu_anti_quant.default)
 def convert_npu_anti_quant_default(
@@ -26,24 +26,24 @@ def convert_npu_anti_quant_default(
     scale: Tensor,
     *,
     offset: Optional[Tensor] = None,
-    dstDtype: Optional[int] = None,
-    srcDtype: Optional[int] = None,
+    dst_dtype: Optional[int] = None,
+    src_dtype: Optional[int] = None,
     meta_outputs: TensorSpec = None
 ):
-    """NB: npu::npu_anti_quant(Tensor x, Tensor scale, *, Tensor? offset, ScalarType? dstDtype,
-                               ScalarType? srcDtype) -> (Tensor)"""
+    """NB: npu::npu_anti_quant(Tensor x, Tensor scale, *, Tensor? offset, ScalarType? dst_dtype,
+                               ScalarType? src_dtype) -> (Tensor)"""
     if x.dtype != DataType.DT_INT8:
         raise RuntimeError("AntiQuant only support int8 for input x")
 
-    if srcDtype is not None:
-        if srcDtype != torch.int8:
-            raise NotImplementedError("AntiQuant only support int8 for srcDtype")
+    if src_dtype is not None:
+        if src_dtype != torch.int8:
+            raise NotImplementedError("AntiQuant only support int8 for src_dtype")
 
-        if srcDtype != x.dtype:
-            x.set_torch_dtype(srcDtype)
+        if src_dtype != x.dtype:
+            x.set_torch_dtype(src_dtype)
 
     dst_type = DataType.DT_FLOAT16
-    if dstDtype is not None:
-        dst_type = torch_type_to_ge_type(dstDtype)
+    if dst_dtype is not None:
+        dst_type = torch_type_to_ge_type(dst_dtype)
     
     return ge.AscendAntiQuantV2(x, scale, offset, dst_type=dst_type, sqrt_mode=False)
