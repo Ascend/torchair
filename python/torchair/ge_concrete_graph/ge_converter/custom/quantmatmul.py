c53c02c112573b49f8ec40b8fe6dbc251e4d7bd9
桂若愚 <guiruoyu@huawei.com>
2024-01-20 07:48:29 +0000

    !362 Add and optional input in quant_batch_matmul Merge pull request !362 from 桂若愚/master

--- python/torchair/ge_concrete_graph/ge_converter/custom/quantmatmul.py
+++ python/torchair/ge_concrete_graph/ge_converter/custom/quantmatmul.py
@@ -20,13 +20,19 @@ from torchair.ge_concrete_graph.fx2ge_converter import declare_supported, regist
 from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
 from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
     Support
+from torchair.ge_concrete_graph.ge_graph import DataType
 
 
-@declare_supported(
-    [
-        Support(I8(8192, 320), I8(320, 2560), F32(1), offset=None, bias=None, transpose_x1=False, transpose_x2=False)
-    ]
-)
+@declare_supported([
+    Support(I8(8192, 320),
+            I8(320, 2560),
+            F32(1),
+            offset=None,
+            bias=None,
+            output_dtype="int8",
+            transpose_x1=False,
+            transpose_x2=False)
+])
 
 @register_fx_node_ge_converter(torch.ops.npu.npu_quant_matmul.default)
 def conveter_npu_npu_quant_matmul(
@@ -35,10 +41,25 @@ def conveter_npu_npu_quant_matmul(
     scale: Tensor,
     offset: Optional[Tensor] = None,
     bias: Optional[Tensor] = None,
+    output_dtype: str = None,
     transpose_x1: bool = False,
     transpose_x2: bool = False,
     meta_outputs: TensorSpec = None
 ):
-    """NB: npu::npu_quant_matmul(Tensor x1, Tensor x2, Tensor scale, Tensor? offset=None, Tensor? bias=None) -> Tensor
+    """NB: npu::npu_quant_matmul(Tensor x1, Tensor x2, Tensor scale, Tensor? offset=None,
+                                 Tensor? bias=None, str? output_dtype=None) -> Tensor
     """
-    return ge.QuantBatchMatmulV3(x1, x2, scale, offset=offset, bias=bias, transpose_x1=False, transpose_x2=False)
+    dtype = DataType.DT_INT8
+    if output_dtype == "float16":
+        dtype = DataType.DT_FLOAT16
+    elif output_dtype == "bfloat16":
+        dtype = DataType.DT_BF16
+
+    return ge.QuantBatchMatmulV3(x1,
+                                 x2,
+                                 scale,
+                                 offset=offset,
+                                 bias=bias,
+                                 dtype=dtype,
+                                 transpose_x1=False,
+                                 transpose_x2=False)
