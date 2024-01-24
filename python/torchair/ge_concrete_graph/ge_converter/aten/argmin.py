96b65ffa73d9c0dd9ef2f88d7659819072367983
张伟康 <zhangweikang10@huawei.com>
2024-01-24 03:21:03 +0000

    !396 [fix] 修复dtype_promote的bug，并增加支持更多Support dtype的test Merge pull request !396 from 张伟康/master

--- python/torchair/ge_concrete_graph/ge_converter/aten/argmin.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/argmin.py
@@ -28,8 +28,17 @@ from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, 
 @declare_supported([
      Support(F32(3, 2)),
      Support(U8(3, 2)),
+     Support(U8(3, 2), 1),
+     Support(U8(3, 2), 1, keepdim=True),
+     Support(I8(3, 2)),
+     Support(I8(3, 2), 1),
+     Support(I8(3, 2), 1, keepdim=True),
+     Support(F16(3, 2)),
      Support(F32(3, 2), 1),
      Support(F32(3, 2), 1, keepdim=True),
+     Support(F16(3, 2), 1),
+     Support(F16(3, 2), 1, keepdim=True),
+     Support(F16(3, 2, 2), 2, keepdim=True),
 ])
 @register_fx_node_ge_converter(torch.ops.aten.argmin.default)
 def conveter_aten_argmin_default(
@@ -43,10 +52,11 @@ def conveter_aten_argmin_default(
         self_cp = ge.Reshape(self, dtype_promote((-1,), target_dtype=DataType.DT_INT64))
         return ge.ArgMin(self_cp, 0)
     if keepdim:
-        index, _ = ge.ArgMinWithValue(self, dimension=dim, keep_dims=keepdim)
-        index = dtype_promote(index, target_dtype=DataType.DT_FLOAT64)
+        self_cp = dtype_promote(self, target_dtype=DataType.DT_FLOAT)
+        index, _ = ge.ArgMinWithValue(self_cp, dimension=dim, keep_dims=keepdim)
+        index = dtype_promote(index, target_dtype=DataType.DT_INT64)
         return index
-    dim = dtype_promote(dim, target_dtype=DataType.DT_FLOAT64)
+    dim = dtype_promote(dim, target_dtype=DataType.DT_INT64)
     return ge.ArgMin(self, dim)
 
 
