ddc3f93ab4eea418315210b0fc94d4a92632a2db
杜承昆 <duchengkun1@huawei.com>
2024-02-04 07:36:35 +0000

    !451 fix index_select dtype bug Merge pull request !451 from 杜承昆/index_select

--- python/torchair/ge_concrete_graph/ge_converter/aten/index_select.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/index_select.py
@@ -19,22 +19,24 @@ from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
 from torchair.ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
-from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
+from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec, DataType
 from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
     Support
 from torchair.ge_concrete_graph.utils import dtype_promote
 
 
 @declare_supported([
-    Support(F32(3, 4), 1, I32(2)),
-    Support(F32(3, 4), -1, I32(2)),
+    Support(F32(3, 4), 1, I32(2, value_range=(0, 4))),
+    Support(F32(3, 4), -1, I32(2, value_range=(0, 4))),
+    Support(F32(3, 10), 1, I32(2, value_range=(0, 10))),
 ])
 @register_fx_node_ge_converter(torch.ops.aten.index_select.default)
 def conveter_aten_index_select_default(
     self: Tensor, dim: int, index: Tensor, meta_outputs: TensorSpec = None
 ):
     """NB: aten::index_select(Tensor self, int dim, Tensor index) -> Tensor"""
-    return ge.GatherV2(self, index, [dim])
+    dim = dtype_promote(dim, target_dtype=DataType.DT_INT64)
+    return ge.GatherV2(self, index, dim)
 
 
 @register_fx_node_ge_converter(torch.ops.aten.index_select.out)
