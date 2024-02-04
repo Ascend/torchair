2417330e3efdcfc65f8b9b6c27d968805f100b2f
zhangqian <zhangqian305@huawei.com>
2024-02-04 07:41:09 +0000

    !452 Index、IndexPut入图——补充头文件维护 Merge pull request !452 from zhangqian/add_head

--- third_party/ascend/include/ops/op_proto/inc/selection.h
+++ third_party/ascend/include/ops/op_proto/inc/selection.h
@@ -19,8 +19,65 @@
  * \brief
  */
 
+
 #ifndef OPS_BUILT_IN_OP_PROTO_INC_SELECTION_H_
 #define OPS_BUILT_IN_OP_PROTO_INC_SELECTION_H_
 
+#include "graph/operator_reg.h"
+namespace ge {
+/**
+* @brief According to the indices and indices_mask, return the value.
 
+* @par Inputs:
+* Four inputs, including:
+* @li x: A ND Tensor.
+* @li indices: Dynamic input. A ND Tensor of int64. return the value according to the indices.
+
+* @par Attributes:
+* @li indices_mask: A list int. Indicates which dimensions of input needs to be indexed.
+
+* @par Outputs:
+* @li y: The indexed output tensor. Has the same type and format as input "x".
+*/
+REG_OP(IndexByTensor)
+    .INPUT(x, TensorType::BasicType())
+    .DYNAMIC_INPUT(indices, TensorType({DT_INT64}))
+    .OUTPUT(y, TensorType::BasicType())
+    .ATTR(indices_mask, ListInt, {})
+    .OP_END_FACTORY_REG(IndexByTensor)
+
+/**
+* @brief According to the index number of indexes, replace the value
+* corresponding to x with the value.
+
+* @par Inputs:
+* Five inputs, including:
+* @li x: A Tensor.
+* @li value: A Tensor of the same type as "x".
+* @li indices: Dynamic input. A Tensor of the indices.
+
+* @par Attributes:
+* @li indices_mask: A list int. Indicates which dimensions of input needs to be indexed.
+* @li accumulate: Does it support self accumulation. Defaults to false.
+* @li unsafe: Does it support throw a RuntimeError when the index is out of range. Defaults to false.
+
+* @par Outputs:
+* @li x: A Tensor.
+
+* @par Third-party framework compatibility
+* Compatible with the Pytorch operator index_put.
+
+* @par Restrictions:
+* Warning:THIS FUNCTION IS EXPERIMENTAL. Please do not use.
+*/
+REG_OP(IndexPutImpl)
+    .INPUT(x, TensorType::BasicType())
+    .INPUT(value, TensorType::BasicType())
+    .DYNAMIC_INPUT(indices, TensorType({DT_INT64}))
+    .OUTPUT(x, TensorType::BasicType())
+    .ATTR(indices_mask, ListInt, {})
+    .ATTR(accumulate, Bool, false)
+    .ATTR(unsafe, Bool, false)
+    .OP_END_FACTORY_REG(IndexPutImpl)
+}  // namespace ge
 #endif  // OPS_BUILT_IN_OP_PROTO_INC_SELECTION_H_
