4a7ddb6955406d6026f7d1b8400aeb7c0b883413
徐立为 <xuliwei4@huawei.com>
2024-02-19 06:20:29 +0000

    !472 avg_pool2d_backward 修复参数缺失的bug Merge pull request !472 from 徐立为/master

--- python/torchair/ge_concrete_graph/ge_converter/aten/avg_pool2d_backward.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/avg_pool2d_backward.py
@@ -18,7 +18,8 @@ import torch
 from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
-from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter, torch_type_to_ge_type, declare_supported
+from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter, torch_type_to_ge_type, \
+    declare_supported
 from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
 from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
     Support
@@ -47,9 +48,9 @@ def conveter_aten_avg_pool2d_backward_default(
     kernelSize = [1, 1, kernel_size[0], kernel_size[1]]
     pads = [padding[0], padding[0], padding[1], padding[1]]
     exclusive = False if count_include_pad else True
-    output = ge.AvgPoolV2Grad(ge.Shape(self), grad_output, ksize=kernelSize, \
-                             padding_mode="CALCULATED", pads=pads, data_format="NCHW", \
-                             global_pooling=False, ceil_mode=ceil_mode, exclusive=exclusive)
+    output = ge.AvgPoolV2Grad(ge.Shape(self), grad_output, ksize=kernelSize, strides=strides_size,
+                              padding_mode="CALCULATED", pads=pads, data_format="NCHW",
+                              global_pooling=False, ceil_mode=ceil_mode, exclusive=exclusive)
     output._node.input_desc[1].layout = "NCHW"
     output._node.output_desc[0].layout = "NCHW"
     return output
@@ -69,4 +70,5 @@ def conveter_aten_avg_pool2d_backward_grad_input(
     meta_outputs: TensorSpec = None
 ):
     """NB: aten::avg_pool2d_backward.grad_input(Tensor grad_output, Tensor self, int[2] kernel_size, int[2] stride, int[2] padding, bool ceil_mode, bool count_include_pad, int? divisor_override, *, Tensor(a!) grad_input) -> Tensor(a!)"""
-    raise NotImplementedError("torch.ops.aten.avg_pool2d_backward.grad_input ge_converter is not implemented!")
+    raise AssertionError("torch.ops.aten.avg_pool2d_backward.grad_input is redundant before pytorch 2.1.0, "
+                         "might be supported in future version.")
