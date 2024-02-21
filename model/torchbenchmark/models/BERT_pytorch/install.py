9d042ee9192c6ec1a63ef4ef1bf0e4723cdcc842
杜承昆 <duchengkun1@huawei.com>
2024-02-21 08:50:10 +0000

    !411 add model directory Merge pull request !411 from 杜承昆/torchbench

---
+++ model/torchbenchmark/models/BERT_pytorch/install.py
@@ -0,0 +1,9 @@
+import subprocess
+import sys
+
+
+def setup_install():
+    subprocess.check_call([sys.executable, '-m', "pip", 'install', '-e', '.'])
+
+if __name__ == '__main__':
+    setup_install()
---
+++ model/torchbenchmark/util/framework/timm/model_factory.py
@@ -0,0 +1,88 @@
+from contextlib import suppress
+import torch
+import typing
+import timm
+from torchbenchmark.util.model import BenchmarkModel
+from .timm_config import TimmConfig
+from typing import Generator, Tuple, Optional
+
+class TimmModel(BenchmarkModel):
+    # To recognize this is a timm model
+    TIMM_MODEL = True
+    # These two variables should be defined by subclasses
+    DEFAULT_TRAIN_BSIZE = None
+    DEFAULT_EVAL_BSIZE = None
+    # Default eval precision on CUDA device is fp16
+    DEFAULT_EVAL_CUDA_PRECISION = "fp16"
+
+    def __init__(self, model_name, test, device, batch_size=None, extra_args=[]):
+        super().__init__(test=test, device=device, batch_size=batch_size, extra_args=extra_args)
+        torch.backends.cudnn.deterministic = False
+        torch.backends.cudnn.benchmark = True
+
+        self.model = timm.create_model(model_name, pretrained=False, scriptable=True)
+        self.cfg = TimmConfig(model = self.model, device = device)
+        self.example_inputs = self._gen_input(self.batch_size)
+
+        self.model.to(
+            device=self.device
+        )
+        if test == "train":
+            self.model.train()
+        elif test == "eval":
+            self.model.eval()
+        self.amp_context = suppress
+
+    def get_input_iter(self):
+        """Yield randomized batch size of inputs."""
+        import math, random
+        n = int(math.log2(self.batch_size))
+        buckets = [2**n for n in range(n)]
+        while True:
+            random_batch_size = random.choice(buckets)
+            example_input = (self._gen_input(random_batch_size), )
+            yield example_input
+
+    def _gen_input(self, batch_size):
+        return torch.randn((batch_size,) + self.cfg.input_size, device=self.device)
+
+    def _gen_target(self, batch_size):
+        return torch.empty(
+            (batch_size,) + self.cfg.target_shape,
+            device=self.device, dtype=torch.long).random_(self.cfg.num_classes)
+
+    def _step_train(self):
+        self.cfg.optimizer.zero_grad()
+        with self.amp_context():
+            output = self.model(self.example_inputs)
+        if isinstance(output, tuple):
+            output = output[0]
+        target = self._gen_target(output.shape[0])
+        self.cfg.loss(output, target).backward()
+        self.cfg.optimizer.step()
+
+    def _step_eval(self):
+        output = self.model(self.example_inputs)
+        return output
+
+    def get_optimizer(self):
+        return self.cfg.optimizer
+
+    def set_optimizer(self, optimizer) -> None:
+        self.cfg.optimizer = optimizer
+
+    def enable_channels_last(self):
+        self.model = self.model.to(memory_format=torch.channels_last)
+        self.example_inputs = self.example_inputs.contiguous(memory_format=torch.channels_last)
+
+    def get_module(self):
+        return self.model, (self.example_inputs,)
+
+    def train(self):
+        self._step_train()
+
+    def eval(self) -> typing.Tuple[torch.Tensor]:
+        with torch.no_grad():
+            with self.amp_context():
+                out = self._step_eval()
+        return (out, )
