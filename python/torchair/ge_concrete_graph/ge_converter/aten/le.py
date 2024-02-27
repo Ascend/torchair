d6427593c3d249322797787081d2f542fbfdb6bb
张伟康 <zhangweikang10@huawei.com>
2024-02-27 02:54:51 +0000

    !434 le_scalar converter add Merge pull request !434 from 张伟康/master

--- python/torchair/ge_concrete_graph/ge_converter/aten/le.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/le.py
@@ -20,11 +20,12 @@ from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float
 from torchair.ge_concrete_graph import ge_apis as ge
 from torchair.ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
 from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
-from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
-    Support
+from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, BF16, F64, I32, I16, I64, I8, U8, \
+    BOOL, Support
 from torchair.ge_concrete_graph.utils import dtype_promote
 
-@declare_supported([
+
+@declare_supported([    
     Support(F32(2, 3), F32(2, 3)),
 ])
 @register_fx_node_ge_converter(torch.ops.aten.le.Tensor)
@@ -33,12 +34,18 @@ def conveter_aten_le_Tensor(self: Tensor, other: Tensor, meta_outputs: TensorSpe
     return ge.LessEqual(self, other)
 
 
+@declare_supported([
+    Support(F32(1024, 1024), 0),
+    Support(F16(1024, 1024), 1.0),
+    Support(BF16(1024, 1024), 1.0),
+])
 @register_fx_node_ge_converter(torch.ops.aten.le.Scalar)
 def conveter_aten_le_Scalar(
     self: Tensor, other: Union[Number, Tensor], meta_outputs: TensorSpec = None
 ):
     """NB: aten::le.Scalar(Tensor self, Scalar other) -> Tensor"""
-    raise NotImplementedError("torch.ops.aten.le.Scalar ge_converter is not implemented!")
+    other = dtype_promote(other, target_dtype=self.dtype)
+    return ge.LessEqual(self, other)
 
 
 @register_fx_node_ge_converter(torch.ops.aten.le.Scalar_out)
