2401ff3238a1163f0268875b689997d815374bc1
liujie379908 <liujie81@huawei.com>
2024-03-05 02:30:23 +0000

    !515 motify threshold and add test case Merge pull request !515 from liujie379908/master

--- python/torchair/ge_concrete_graph/ge_converter/aten/threshold.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/threshold.py
@@ -18,10 +18,18 @@ import torch
 from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
-from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter
+from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter, declare_supported
 from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
+from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
+    Support
 
 
+@declare_supported([
+    Support(F32(2, 2), 0., 0),
+    Support(F32(2, 2), 0.1, 20),
+    Support(F16(2, 2), 0., 0),
+    Support(F16(2, 2), 0.1, 20),
+])
 @register_fx_node_ge_converter(torch.ops.aten.threshold.default)
 def conveter_aten_threshold_default(
     self: Tensor,
@@ -30,7 +38,7 @@ def conveter_aten_threshold_default(
     meta_outputs: TensorSpec = None,
 ):
     """NB: aten::threshold(Tensor self, Scalar threshold, Scalar value) -> Tensor"""
-    raise NotImplementedError("torch.ops.aten.threshold.default ge_converter is not implemented!")
+    return ge.ThresholdV2(self, threshold=threshold, value=value)
 
 
 @register_fx_node_ge_converter(torch.ops.aten.threshold.out)
