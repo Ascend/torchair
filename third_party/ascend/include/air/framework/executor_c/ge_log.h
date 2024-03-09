b5a230dc94a3ad2e6f707ed232afed680a638c24
刘志勇 <liuzhiyong46@hisilicon.com>
2024-03-09 07:11:35 +0000

    !551 【轻量级 PR】：update third_party/ascend/include/air/framework/executor_c/ge_log.h. Merge pull request !551 from 刘志勇/N/A

--- third_party/ascend/include/air/framework/executor_c/ge_log.h
+++ third_party/ascend/include/air/framework/executor_c/ge_log.h
@@ -98,33 +98,33 @@ do {                                                                            
 #else
 #define GELOGD(fmt, ...)                                                                           \
   do {                                                                                             \
-    DlogWrite(GE_MODULE_NAME, DLOG_DEBUG, "[DEBUG][%s:%d]%ld " fmt "\n", __FILE__, __LINE__,       \
-              (long int)GetTid(), ##__VA_ARGS__);                                                  \
+    DlogRecord(GE_MODULE_NAME, DLOG_DEBUG, "[DEBUG][%s:%d]%ld " fmt "\n", __FILE__, __LINE__,       \
+               (long int)GetTid(), ##__VA_ARGS__);                                                  \
   } while (false)
 
 #define GELOGI(fmt, ...)                                                                           \
   do {                                                                                             \
-    DlogWrite(GE_MODULE_NAME, DLOG_INFO, "[INFO][%s:%d]%ld " fmt "\n", __FILE__, __LINE__,         \
-              (long int)GetTid(), ##__VA_ARGS__);                                                  \
+    DlogRecord(GE_MODULE_NAME, DLOG_INFO, "[INFO][%s:%d]%ld " fmt "\n", __FILE__, __LINE__,         \
+               (long int)GetTid(), ##__VA_ARGS__);                                                  \
   } while (false)
 
 #define GELOGW(fmt, ...)                                                                           \
   do {                                                                                             \
-    DlogWrite(GE_MODULE_NAME, DLOG_WARN, "[WARNING][%s:%d]%ld " fmt "\n", __FILE__, __LINE__,      \
-              (long int)GetTid(), ##__VA_ARGS__);                                                  \
+    DlogRecord(GE_MODULE_NAME, DLOG_WARN, "[WARNING][%s:%d]%ld " fmt "\n", __FILE__, __LINE__,      \
+               (long int)GetTid(), ##__VA_ARGS__);                                                  \
   } while (false)
 
 #define GEEVENT(fmt, ...)                                                                          \
   do {                                                                                             \
-    DlogWrite(GE_MODULE_NAME, DLOG_EVENT, "[Event][%s:%d]%ld " fmt "\n", __FILE__, __LINE__,       \
-              (long int)GetTid(), ##__VA_ARGS__);                                                  \
+    DlogRecord(GE_MODULE_NAME, DLOG_EVENT, "[Event][%s:%d]%ld " fmt "\n", __FILE__, __LINE__,       \
+               (long int)GetTid(), ##__VA_ARGS__);                                                  \
   } while (false)
 
 #define GELOGE(ERROR_CODE, fmt, ...)                                                               \
   do {                                                                                             \
-    DlogWrite(GE_MODULE_NAME, DLOG_ERROR, "[ERROR][%s:%d]%ld:ErrorNo:%u(%s)%s " fmt "\n",          \
-              __FILE__, __LINE__, (long int)GetTid(), (uint32_t)ERROR_CODE,                        \
-              GE_GET_ERRORNO_STR, GE_GET_ERROR_LOG_HEADER, ##__VA_ARGS__);                         \
+    DlogRecord(GE_MODULE_NAME, DLOG_ERROR, "[ERROR][%s:%d]%ld:ErrorNo:%u(%s)%s " fmt "\n",          \
+               __FILE__, __LINE__, (long int)GetTid(), (uint32_t)ERROR_CODE,                        \
+               GE_GET_ERRORNO_STR, GE_GET_ERROR_LOG_HEADER, ##__VA_ARGS__);                         \
   } while (false)
 #endif
 
