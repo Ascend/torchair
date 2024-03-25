d96a54ff1ac964fdfb2a6de7229f20a5c9a28e67
陈睿敏 <chenruimin2@huawei.com>
2024-03-25 07:24:01 +0000

    !479 [Converter] improve performance for slice_scatter converter Merge pull request !479 from 陈睿敏/master

--- python/torchair/ge_concrete_graph/ge_converter/aten/slice_scatter.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/slice_scatter.py
@@ -23,10 +23,47 @@ from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_conve
 from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec, torch_type_to_ge_type
 from torchair.ge_concrete_graph.utils import dtype_promote
 from torchair.ge_concrete_graph.supported_declaration import F32, F16, Support
+from torchair.ge_concrete_graph.ge_converter.aten.slice_backward import check_if_skip
+
+
+def static_slice_scatter_step1(
+    self: Tensor,
+    src: Tensor,
+    dim: int = 0,
+    start: Optional[Union[int, Tensor]] = None,
+    end: Optional[Union[int, Tensor]] = None,
+    step: Union[int, Tensor] = 1,
+):
+    if check_if_skip(start, end, step):
+        return src
+    int_max = 2147483647
+    if not isinstance(end, Tensor) and end != sys.maxsize and end > int_max:
+        raise NotImplementedError("ge.StridedSliceV2 does not support shapes exceeding the INT32_MAX!")
+
+    dim = dim % self.rank
+    limit = self.symsize[dim]
+    if end < 0:
+        end = end % limit
+    else:
+        end = min(end, int_max)
+
+    if start < 0:
+        start = start % limit
+    else:
+        start = min(start, int_max)
+    
+    start = min(start, end)
+    to_pad = ge.StridedSliceV2(self, 0, start, axes=dim, strides=step)
+    to_pad2 = ge.StridedSliceV2(self, end, int_max, axes=dim, strides=step)
+    output = ge.ConcatV2([to_pad, src, to_pad2], concat_dim=dim, N=3)
+    return output
 
 @declare_supported([
     Support(F32(22), F32(10), 0, 0, 10),
     Support(F32(10), F32(10), 0, 0, 20),
+    Support(F32(10, 10), F32(10, 2), 1, 1, 3),
+    Support(F32(10, 10), F32(10, 1), 1, -1, 9223372036854775807),
+    Support(F32(10, 10), F32(10, 10), 1, 0, 9223372036854775807),
 ])
 @register_fx_node_ge_converter(torch.ops.aten.slice_scatter.default)
 def conveter_aten_slice_scatter_default(
@@ -39,6 +76,9 @@ def conveter_aten_slice_scatter_default(
     meta_outputs: TensorSpec = None,
 ):
     """NB: aten::slice_scatter(Tensor self, Tensor src, int dim=0, SymInt? start=None, SymInt? end=None, SymInt step=1) -> Tensor"""
+    if not (isinstance(start, Tensor) or isinstance(end, Tensor) or \
+        isinstance(step, Tensor)) and step == 1:
+        static_slice_scatter_step1(self, src, dim, start, end, step)
     if start is None:
         start = 0
     if end is None:
