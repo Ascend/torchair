6525b03a3c1803c5277634defe53ac1cd47290f2
鲁博洋 <luboyang@huawei.com>
2024-03-30 07:46:30 +0000

    !683 logical_or convert Merge pull request !683 from 鲁博洋/logical_or

--- python/torchair/ge_concrete_graph/ge_converter/aten/logical_or.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/logical_or.py
@@ -18,16 +18,23 @@ import torch
 from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair.ge_concrete_graph import ge_apis as ge
-from torchair.ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter
+from torchair.ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
 from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
+from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
+    Support
 
 
+@declare_supported(
+    [
+        Support(BOOL(2, 2), BOOL(2, 2)),
+    ]
+)
 @register_fx_node_ge_converter(torch.ops.aten.logical_or.default)
 def conveter_aten_logical_or_default(
     self: Tensor, other: Tensor, meta_outputs: TensorSpec = None
 ):
     """NB: aten::logical_or(Tensor self, Tensor other) -> Tensor"""
-    raise NotImplementedError("torch.ops.aten.logical_or.default ge_converter is not implemented!")
+    return ge.LogicalOr(self, other)
 
 
 @register_fx_node_ge_converter(torch.ops.aten.logical_or.out)
