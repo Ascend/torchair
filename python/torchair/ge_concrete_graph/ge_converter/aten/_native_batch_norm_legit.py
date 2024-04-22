c50dcccd1f84dde043bbff0faafbd65d9549d85b
zhangqian <zhangqian305@huawei.com>
2024-04-22 06:31:30 +0000

    !730 [add] add conveter:_native_batch_norm_legit.no_status Merge pull request !730 from zhangqian/batch_norm_legit

--- python/torchair/ge_concrete_graph/ge_converter/aten/_native_batch_norm_legit.py
+++ python/torchair/ge_concrete_graph/ge_converter/aten/_native_batch_norm_legit.py
@@ -102,7 +102,51 @@ def conveter_aten__native_batch_norm_legit_no_stats(
     meta_outputs: TensorSpec = None,
 ):
     """NB: aten::_native_batch_norm_legit.no_stats(Tensor input, Tensor? weight, Tensor? bias, bool training, float momentum, float eps) -> (Tensor, Tensor, Tensor)"""
-    raise NotImplementedError("torch.ops.aten._native_batch_norm_legit.no_stats ge_converter is not implemented!")
+    if not training:
+        raise RuntimeError(
+            "torch.ops.aten._native_batch_norm_legit.no_stats ge_converter is not implemented while training is False!"
+        )
+    dim = input.rank
+    if dim > 5 or dim < 2:
+        raise RuntimeError("torch.ops.aten._native_batch_norm_legit.no_stats "
+                           "ge_converter is not implemented while input dim > 5 or input dim <2!")
+    input_size = ge.Shape(input, dtype=DataType.DT_INT32)
+    if weight is None:
+        weight = ge.Fill(ge.Gather(input_size, 1), ge.Cast(1., dst_type=input.dtype))
+
+    if bias is None:
+        bias = ge.Fill(ge.Gather(input_size, 1), ge.Cast(0., dst_type=input.dtype))
+
+    running_mean = ge.Fill(ge.Gather(input_size, 1), ge.Cast(0., dst_type=input.dtype))
+    running_var = ge.Fill(ge.Gather(input_size, 1), ge.Cast(1., dst_type=input.dtype))
+    if dim <= 4:
+        if dim == 2 or dim == 3:
+            input = ge.Unsqueeze(input, axes=list(range(dim, 4)))
+        sum_output, square_sum = ge.BNTrainingReduce(input)
+        specific_op_input_layout(sum_output, indices=0, layout="NCHW")
+        specific_op_output_layout(sum_output, indices=[0, 1], layout="NCHW")
+        output, mean, variance, batch_mean, batch_variance = ge.BNTrainingUpdate(input, sum_output,
+                                                                                 square_sum, weight, bias, running_mean,
+                                                                                 running_var, factor=momentum,
+                                                                                 epsilon=eps)
+        specific_op_input_layout(output, indices=list(range(7)), layout="NCHW")
+        specific_op_output_layout(output, indices=list(range(5)), layout="NCHW")
+        if dim == 2 or dim == 3:
+            output = ge.Squeeze(output, axis=list(range(3, dim - 1, -1)))
+        result = (output, batch_mean, batch_variance)
+        return result
+
+    sum_output, square_sum = ge.BN3DTrainingReduce(input)
+    specific_op_input_layout(sum_output, indices=0, layout="NCDHW")
+    specific_op_output_layout(sum_output, indices=[0, 1], layout="NCDHW")
+    output, mean, variance, batch_mean, batch_variance = ge.BN3DTrainingUpdate(input, sum_output,
+                                                                               square_sum, weight, bias, running_mean,
+                                                                               running_var, factor=momentum,
+                                                                               epsilon=eps)
+    specific_op_input_layout(output, indices=list(range(7)), layout="NCDHW")
+    specific_op_output_layout(output, indices=list(range(5)), layout="NCDHW")
+    result = (output, batch_mean, batch_variance)
+    return result
 
 
 @register_fx_node_ge_converter(torch.ops.aten._native_batch_norm_legit.out)
