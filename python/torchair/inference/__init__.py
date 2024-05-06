89952aed4b7fd7f3bd8de782d46cf4b7625b599f
薛鹏 <xuepeng4@huawei.com>
2024-05-06 02:54:53 +0000

    !791 暂不开放inference缓存编译相关接口 Merge pull request !791 from 薛鹏/master

--- python/torchair/inference/__init__.py
+++ python/torchair/inference/__init__.py
@@ -4,3 +4,5 @@ from torchair.inference.cache_compiler import NoGuardCompiledMethod
 from torchair.inference.cache_compiler import set_dim_gears
 from torchair.inference.cache_compiler import get_dim_gears
 from torchair.inference.cache_compiler import readable_cache
+
+__all__ = []
