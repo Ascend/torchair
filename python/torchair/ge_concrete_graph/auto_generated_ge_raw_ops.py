2849fde420277415d2afb448ddf612cd29c5da1d
陈建军 <chenjianjun11@huawei.com>
2024-05-07 07:33:23 +0000

    !782 add AscendQuantV2 Merge pull request !782 from 陈建军/master

--- python/torchair/ge_concrete_graph/auto_generated_ge_raw_ops.py
+++ python/torchair/ge_concrete_graph/auto_generated_ge_raw_ops.py
@@ -56027,6 +56027,57 @@ def AscendQuant(x: Tensor, *, scale: float, offset: float, sqrt_mode: bool=False
     # return outputs
     return y
 
+# This api is auto-generated from IR AscendQuantV2
+@auto_convert_to_tensor([False, False, False], [False, False, True])
+def AscendQuantV2(x: Tensor, scale: Tensor, offset: Optional[Tensor], *, sqrt_mode: bool=False, round_mode: str="round", dst_type: int=2, dependencies=[], node_name=None):
+    """REG_OP(AscendQuantV2)\n
+.INPUT(x, TensorType({DT_FLOAT16, DT_FLOAT, DT_BF16}))\n
+.INPUT(scale, TensorType({DT_FLOAT16, DT_FLOAT, DT_BF16}))\n
+.OPTIONAL_INPUT(offset, TensorType({DT_FLOAT16, DT_FLOAT, DT_BF16}))\n
+.OUTPUT(y, TensorType({DT_INT8, DT_INT4}))\n
+.ATTR(sqrt_mode, Bool, false)\n
+.ATTR(round_mode, String, "round")\n
+.ATTR(dst_type, Int, DT_INT8)\n
+"""
+
+    op = get_default_ge_graph().op.add()
+    op.type = "AscendQuantV2"
+    op.name = next_unique_name(node_name, "AscendQuantV2")
+
+    # process dependices
+    for dependency in dependencies:
+        op.input.append(dependency.controller)
+
+    # process inputs
+    op.input.append(x.tensor)
+    op.input_desc.add().CopyFrom(x.desc)
+    op.input_desc[-1].name = "x"
+    op.input.append(scale.tensor)
+    op.input_desc.add().CopyFrom(scale.desc)
+    op.input_desc[-1].name = "scale"
+    if offset is not None:
+        op.input.append(offset.tensor)
+        op.input_desc.add().CopyFrom(offset.desc)
+        op.input_desc[-1].name = "offset"
+    else:
+        op.input.append('')
+        op.input_desc.add().CopyFrom(get_invalid_desc())
+        op.input_desc[-1].name = "offset"
+
+    # process attrs
+    op.attr["sqrt_mode"].b = sqrt_mode
+    op.attr["round_mode"].s = compat_as_bytes(round_mode)
+    op.attr["dst_type"].i = dst_type
+
+    # process outputs
+    output_index = 0
+    op.output_desc.add().name = "y"
+    y = Tensor(op, output_index)
+    output_index += 1
+
+    # return outputs
+    return y
+
 
 # This api is auto-generated from IR AscendDequant
 @auto_convert_to_tensor([False, False], [False, False])
--- python/torchair/ge_concrete_graph/ge_converter/custom/quantized.py
+++ python/torchair/ge_concrete_graph/ge_converter/custom/quantized.py
@@ -18,7 +18,8 @@ import torch
 from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme
 from torchair.ge_concrete_graph import ge_apis as ge
-from torchair.ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
+from torchair.ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter, \
+    torch_type_to_ge_type
 from torchair.ge_concrete_graph.ge_graph import Tensor, TensorSpec
 from torchair.ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
     Support
@@ -36,13 +37,18 @@ def conveter_npu_quantize_default(
     zero_points: Optional[Tensor] = None,
     dtype: int = torch.uint8,
     axis: int = 1,
+    div_mode: bool = True,
     meta_outputs: TensorSpec = None
 ):
     """
     NB: aten::quantize_per_channel(Tensor self, Tensor scales, Tensor zero_points,
                                       int axis, ScalarType dtype) -> Tensor
     """
-    
+    if not div_mode:
+        if dtype == torch.qint8:
+            dtype = torch.int8
+        return ge.AscendQuantV2(self, scales, zero_points, sqrt_mode=False, round_mode="round",
+            dst_type=torch_type_to_ge_type(dtype))
     if scales.rank != 1:
         raise RuntimeError("Scales' dim should be equal to 1.")
     if axis < 0:
