68dbb72b6f631bd1c2a0029f65b0f6a9374de472
张伟康 <zhangweikang10@huawei.com>
2024-05-25 09:30:34 +0000

    !841 【Fix】some error import custom in high pytorch version Merge pull request !841 from 张伟康/tng_custom3

--- python/torchair/ge_concrete_graph/ge_converter/custom/__init__.py
+++ python/torchair/ge_concrete_graph/ge_converter/custom/__init__.py
@@ -1,7 +1,13 @@
 import os
 import pkgutil
+import importlib
 
 
 __all__ = list(module for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]))
 
-from . import *
+for name in __all__:
+    module_name = "torchair.ge_concrete_graph.ge_converter.custom." + name
+    try:
+        importlib.import_module(module_name)
+    except AttributeError:
+        pass
