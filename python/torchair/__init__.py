10aea8ffcda1adb8c396869fd4219b8e5d01759f
杜承昆 <duchengkun1@huawei.com>
2024-05-27 11:57:37 +0000

    !842 重命名dynamo_export.py以解决接口跟文件重名问题 Merge pull request !842 from 杜承昆/dynamo-export

--- python/torchair/__init__.py
+++ python/torchair/__init__.py
@@ -4,7 +4,7 @@ import pkg_resources
 
 from torchair.npu_fx_compiler import get_compiler
 from torchair.npu_fx_compiler import get_npu_backend
-from torchair.dynamo_export import dynamo_export
+from torchair.npu_export import dynamo_export
 from torchair.configs.compiler_config import CompilerConfig
 from torchair.ge_concrete_graph import ge_converter
 from torchair.experimental.inference import use_internal_format_weight
