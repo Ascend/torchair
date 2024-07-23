670aa74bd2927730f9b1755e07bd0ea84f339785
徐立为 <xuliwei4@huawei.com>
2024-07-23 07:59:32 +0000

    !1017 fix bug: converter minimum Merge pull request !1017 from 徐立为/master

--- python/torchair/_ge_concrete_graph/ge_converter/aten/minimum.py
+++ python/torchair/_ge_concrete_graph/ge_converter/aten/minimum.py
@@ -19,7 +19,7 @@ from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair._ge_concrete_graph import ge_apis as ge
 from torchair._ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
-from torchair.ge._ge_graph import Tensor, TensorSpec
+from torchair.ge._ge_graph import Tensor, TensorSpec, DataType
 from torchair._ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
     Support
 from torchair._ge_concrete_graph.utils import dtype_promote
@@ -38,6 +38,8 @@ def conveter_aten_minimum_default(
     self: Tensor, other: Tensor, meta_outputs: TensorSpec = None
 ):
     """NB: aten::minimum(Tensor self, Tensor other) -> Tensor"""
+    if meta_outputs.dtype == DataType.DT_BOOL:
+        return ge.Mul(self, other)
     self, other = dtype_promote(self, other, target_dtype=meta_outputs.dtype)
     return ge.Minimum(self, other)
 
