cf4b288bd9d9b8eff852130243e9773c080417b7
徐立为 <xuliwei4@huawei.com>
2024-07-29 09:13:54 +0000

    !1034 update conveter: minimum Merge pull request !1034 from 徐立为/master

--- python/torchair/_ge_concrete_graph/ge_converter/aten/minimum.py
+++ python/torchair/_ge_concrete_graph/ge_converter/aten/minimum.py
@@ -32,6 +32,10 @@ from torchair._ge_concrete_graph.utils import dtype_promote
     Support(I8(32), BOOL(32)),
     Support(F16(32), BOOL(32)),
     Support(I64(32), I8(32)),
+    Support(U8(32), I8(32)),
+    Support(U8(32), U8(32)),
+    Support(I8(32), I16(32)),
+    Support(BOOL(32), BOOL(32)),
 ])
 @register_fx_node_ge_converter(torch.ops.aten.minimum.default)
 def conveter_aten_minimum_default(
@@ -40,6 +44,10 @@ def conveter_aten_minimum_default(
     """NB: aten::minimum(Tensor self, Tensor other) -> Tensor"""
     if meta_outputs.dtype == DataType.DT_BOOL:
         return ge.Mul(self, other)
+    if meta_outputs.dtype in [DataType.DT_UINT8, DataType.DT_INT16]:
+        self, other = dtype_promote(self, other, target_dtype=DataType.DT_INT32)
+        outputs = ge.Minimum(self, other)
+        return dtype_promote(outputs, target_dtype=meta_outputs.dtype)
     self, other = dtype_promote(self, other, target_dtype=meta_outputs.dtype)
     return ge.Minimum(self, other)
 
