eb7e8ee6e5e76328f09a26d7b33424a426286dc8
张伟康 <zhangweikang10@huawei.com>
2024-08-17 03:18:47 +0000

    !1048 增加优化器for-loop场景下缺失的部分converter(lerp&sign) Merge pull request !1048 from 张伟康/forloop

--- python/torchair/_ge_concrete_graph/ge_converter/aten/lerp.py
+++ python/torchair/_ge_concrete_graph/ge_converter/aten/lerp.py
@@ -18,16 +18,24 @@ import torch
 from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair._ge_concrete_graph import ge_apis as ge
-from torchair._ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter
+from torchair._ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
 from torchair.ge._ge_graph import Tensor, TensorSpec
+from torchair._ge_concrete_graph.supported_declaration import F32, F16, BF16, Support
 
 
+@declare_supported(
+    [
+        Support(F32(1024, 1024), F32(1024, 1024), 0.5),
+        Support(F16(5,), F16(5,), 0.5),
+        Support(BF16(5,), BF16(5,), 0.5),
+    ]
+)
 @register_fx_node_ge_converter(torch.ops.aten.lerp.Scalar)
 def conveter_aten_lerp_Scalar(
     self: Tensor, end: Tensor, weight: Union[Number, Tensor], meta_outputs: TensorSpec = None
 ):
     """NB: aten::lerp.Scalar(Tensor self, Tensor end, Scalar weight) -> Tensor"""
-    raise NotImplementedError("torch.ops.aten.lerp.Scalar ge_converter is not implemented!")
+    return ge.Lerp(self, end, weight)
 
 
 @register_fx_node_ge_converter(torch.ops.aten.lerp.Tensor)
--- python/torchair/_ge_concrete_graph/ge_converter/aten/sign.py
+++ python/torchair/_ge_concrete_graph/ge_converter/aten/sign.py
@@ -18,14 +18,22 @@ import torch
 from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair._ge_concrete_graph import ge_apis as ge
-from torchair._ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter
+from torchair._ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
 from torchair.ge._ge_graph import Tensor, TensorSpec
+from torchair._ge_concrete_graph.supported_declaration import F32, F16, BF16, I32, I16, I64, I8, U8, Support
 
 
+@declare_supported(
+    [
+        Support(F32(3, 3)),
+        Support(F16(1,)),
+        Support(I64(1,)),
+    ]
+)
 @register_fx_node_ge_converter(torch.ops.aten.sign.default)
 def conveter_aten_sign_default(self: Tensor, meta_outputs: TensorSpec = None):
     """NB: aten::sign(Tensor self) -> Tensor"""
-    raise NotImplementedError("torch.ops.aten.sign.default ge_converter is not implemented!")
+    return ge.Sign(self)
 
 
 @register_fx_node_ge_converter(torch.ops.aten.sign.out)
