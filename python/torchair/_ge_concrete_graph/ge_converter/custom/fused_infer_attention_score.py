f2cb2ec84261800ca662757f65e23aaa29c88222
dev_ZUT <liangzhihao11@huawei.com>
2024-08-29 06:54:41 +0000

    !1087 ifa w4a16 kvantiquant Merge pull request !1087 from dev_ZUT/master

--- python/torchair/_ge_concrete_graph/ge_converter/custom/fused_infer_attention_score.py
+++ python/torchair/_ge_concrete_graph/ge_converter/custom/fused_infer_attention_score.py
@@ -80,6 +80,35 @@ def convert_npu_npu_fused_infer_attention_score(
     value_antiquant_mode: int = 0,
     meta_outputs: TensorSpec = None,
 ):
+    if input_layout == 'BSH':
+        const = ge.Const([1, 1, 8])
+    else:
+        const = ge.Const([1, 1, 1, 8])
+    if key is not None and key.dtype == DataType.DT_INT32:
+        shape = ge.Shape(key)
+        key_shape = ge.Mil(shape, const)
+        key = ge.Bitcast(key, type=DataType.DT_INT4)
+        key = ge.Reshape(key, key_shape)
+ 
+    if value is not None and value.dtype == DataType.DT_INT32:
+        shape = ge.Shape(value)
+        value_shape = ge.Mil(shape, const)
+        value = ge.Bitcast(value, type=DataType.DT_INT4)
+        value = ge.Reshape(value, value_shape)
+ 
+    if key_shared_prefix is not None and key_shared_prefix.dtype == DataType.DT_INT32:
+        shape = ge.Shape(key_shared_prefix)
+        key_shared_prefix_shape = ge.Mil(shape, const)
+        key_shared_prefix = ge.Bitcast(key_shared_prefix, type=DataType.DT_INT4)
+        key_shared_prefix = ge.Reshape(key_shared_prefix, key_shared_prefix_shape)
+ 
+    if value_shared_prefix is not None and value_shared_prefix.dtype == DataType.DT_INT32:
+        shape = ge.Shape(value_shared_prefix)
+        value_shared_prefix_shape = ge.Mil(shape, const)
+        value_shared_prefix = ge.Bitcast(value_shared_prefix, type=DataType.DT_INT4)
+        value_shared_prefix = ge.Reshape(value_shared_prefix, value_shared_prefix_shape)
+ 
+
     key_list = [key]
     value_list = [value]
     if actual_seq_lengths is not None:
