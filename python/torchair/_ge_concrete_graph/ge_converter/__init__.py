a0189dcf05d0ddb69f24aecf1ced1e3a4d6e3f0f
杜承昆 <duchengkun1@huawei.com>
2024-10-28 07:46:34 +0000

    !1213 Suppress import error for pytorch master. Merge pull request !1213 from 杜承昆/prims

--- python/torchair/_ge_concrete_graph/ge_converter/__init__.py
+++ python/torchair/_ge_concrete_graph/ge_converter/__init__.py
@@ -6,10 +6,18 @@ from torchair._ge_concrete_graph.ge_converter.quantized import *
 from torchair._ge_concrete_graph.ge_converter.rngprims import *
 from torchair._ge_concrete_graph.ge_converter.c10d_functional import *
 import torchair._ge_concrete_graph.ge_converter.builtin_converters
-import torchair._ge_concrete_graph.ge_converter.aten as aten
-for name in aten.__all__:
-    module_name = "torchair._ge_concrete_graph.ge_converter.aten." + name
-    try:
-        importlib.import_module(module_name)
-    except AttributeError:
-        pass
+
+
+def try_import_submodule(submodule_name):
+    module_name = f"torchair._ge_concrete_graph.ge_converter.{submodule_name}"
+    submodule = importlib.import_module(module_name)
+    for name in submodule.__all__:
+        file_name = module_name + "." + name
+        try:
+            importlib.import_module(file_name)
+        except AttributeError:
+            pass
+
+
+try_import_submodule("prims")
+try_import_submodule("aten")
