114e06a0b131d4055da3688d915bac4bbd29eb4f
baochangjin <baochangjin2@h-partners.com>
2024-11-21 06:33:10 +0000

    !1248 修改_foreach_round_off_number中几个接口适配cann修改 Merge pull request !1248 from baochangjin/master

--- python/torchair/_ge_concrete_graph/ge_converter/aten/_foreach_round_off_number.py
+++ python/torchair/_ge_concrete_graph/ge_converter/aten/_foreach_round_off_number.py
@@ -46,8 +46,9 @@ def split_float_int_tensor(self: List[Tensor]):
 def conveter_aten__foreach_floor_default(self: List[Tensor], meta_outputs: List[TensorSpec] = None):
     """NB: aten::_foreach_floor(Tensor[] self) -> Tensor[]"""
     int_id, int_tensor, float_tensor = split_float_int_tensor(self)
+    round_mode = ge.Fill(1, ge.Cast(2, dst_type=DataType.DT_INT8))
     if len(float_tensor):
-        out = ge.ForeachRoundOffNumber(float_tensor, roundMode=2)
+        out = ge.ForeachRoundOffNumber(float_tensor, roundMode=round_mode)
         int_idx = range(len(int_id))
         for i in int_idx:
             out.insert(int_id[i], int_tensor[i])
@@ -64,7 +65,8 @@ def conveter_aten__foreach_floor_default(self: List[Tensor], meta_outputs: List[
 def conveter_aten__foreach_frac_default(self: List[Tensor], meta_outputs: List[TensorSpec] = None):
     """NB: aten::_foreach_frac(Tensor[] self) -> Tensor[]"""
     int_id, int_tensor, float_tensor = split_float_int_tensor(self)
-    out = ge.ForeachRoundOffNumber(float_tensor, roundMode=7)
+    round_mode = ge.Fill(1, ge.Cast(7, dst_type=DataType.DT_INT8))
+    out = ge.ForeachRoundOffNumber(float_tensor, roundMode=round_mode)
     int_idx = range(len(int_id))
     for i in int_idx:
         out.insert(int_id[i], ge.ZerosLike(int_tensor[i]))
@@ -78,7 +80,8 @@ def conveter_aten__foreach_frac_default(self: List[Tensor], meta_outputs: List[T
 @register_fx_node_ge_converter(torch.ops.aten._foreach_ceil.default)
 def conveter_aten__foreach_ceil_default(self: List[Tensor], meta_outputs: List[TensorSpec] = None):
     """NB: aten::_foreach_ceil(Tensor[] self) -> Tensor[]"""
-    return ge.ForeachRoundOffNumber(self, roundMode=3)
+    round_mode = ge.Fill(1, ge.Cast(3, dst_type=DataType.DT_INT8))
+    return ge.ForeachRoundOffNumber(self, roundMode=round_mode)
 
 
 @declare_supported([
@@ -89,8 +92,9 @@ def conveter_aten__foreach_ceil_default(self: List[Tensor], meta_outputs: List[T
 def conveter_aten__foreach_round_default(self: List[Tensor], meta_outputs: List[TensorSpec] = None):
     """NB: aten::_foreach_round(Tensor[] self) -> Tensor[]"""
     int_id, int_tensor, float_tensor = split_float_int_tensor(self)
+    round_mode = ge.Fill(1, ge.Cast(4, dst_type=DataType.DT_INT8))
     if len(float_tensor):
-        out = ge.ForeachRoundOffNumber(float_tensor, roundMode=4)
+        out = ge.ForeachRoundOffNumber(float_tensor, roundMode=round_mode)
         int_idx = range(len(int_id))
         for i in int_idx:
             out.insert(int_id[i], int_tensor[i])
@@ -106,4 +110,5 @@ def conveter_aten__foreach_round_default(self: List[Tensor], meta_outputs: List[
 @register_fx_node_ge_converter(torch.ops.aten._foreach_trunc.default)
 def conveter_aten__foreach_trunc_default(self: List[Tensor], meta_outputs: List[TensorSpec] = None):
     """NB: aten::_foreach_trunc(Tensor[] self) -> Tensor[]"""
-    return ge.ForeachRoundOffNumber(self, roundMode=5)
+    round_mode = ge.Fill(1, ge.Cast(5, dst_type=DataType.DT_INT8))
+    return ge.ForeachRoundOffNumber(self, roundMode=round_mode)
