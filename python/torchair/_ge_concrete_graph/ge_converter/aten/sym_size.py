6825f420e7ba53082665276cafb1b1911cb54faa
xudaohong <xudaohong@huawei.com>
2024-12-04 09:41:46 +0000

    !1293 [feat] converter support aten.sym_size.int Merge pull request !1293 from xudaohong/cherry-pick-1733303315

--- python/torchair/_ge_concrete_graph/ge_converter/aten/sym_size.py
+++ python/torchair/_ge_concrete_graph/ge_converter/aten/sym_size.py
@@ -21,6 +21,22 @@ from torchair._ge_concrete_graph import ge_apis as ge
 from torchair._ge_concrete_graph.fx2ge_converter import register_fx_node_ge_converter
 from torchair.ge._ge_graph import DataType, Tensor, TensorSpec
 from torchair._ge_concrete_graph.utils import force_op_unknown_shape
+from torchair._ge_concrete_graph.supported_declaration import F32, F16, I32, I64, Support
+from torchair._ge_concrete_graph.fx2ge_converter import declare_supported
+
+
+def aten_sym_size_int(self: Tensor, dim: int, meta_outputs: TensorSpec = None):
+    """NB: aten::sym_size.int(Tensor self, int dim) -> SymInt"""
+    if isinstance(meta_outputs._meta, int):
+        return meta_outputs._meta
+    if isinstance(meta_outputs._meta, torch.SymInt):
+        try:
+            return int(str(meta_outputs._meta))
+        except ValueError:
+            pass  # Not static dim size
+    shape = ge.Shape(self, dtype=DataType.DT_INT64)
+    # force unknown shape with ge.Gather when parse symsize
+    return force_op_unknown_shape(ge.Gather(shape, dim))
 
 
 @register_fx_node_ge_converter(torch.ops.aten.sym_size.default)
@@ -29,19 +45,28 @@ def conveter_aten_sym_size_default(self: Tensor, meta_outputs: TensorSpec = None
     raise NotImplementedError("torch.ops.aten.sym_size.default ge_converter is not implemented!")
 
 
+@declare_supported([
+    Support(I32(2, 3), 1),
+    Support(I64(3, 2, 2), 1),
+    Support(F16(2, 3), 1),
+    Support(F32(2, 2, 4), 1),
+])
 @register_fx_node_ge_converter(torch.ops.aten.sym_size)
+def conveter_aten_sym_size(self: Tensor, dim: int, meta_outputs: TensorSpec = None):
+    """NB: aten::sym_size(Tensor self, int dim) -> SymInt"""
+    return aten_sym_size_int(self, dim, meta_outputs)
+
+
+@declare_supported([
+    Support(I32(2, 3), 1),
+    Support(I64(3, 2, 2), 1),
+    Support(F16(2, 3), 1),
+    Support(F32(2, 2, 4), 1),
+])
+@register_fx_node_ge_converter(torch.ops.aten.sym_size.int)
 def conveter_aten_sym_size_int(self: Tensor, dim: int, meta_outputs: TensorSpec = None):
     """NB: aten::sym_size.int(Tensor self, int dim) -> SymInt"""
-    if isinstance(meta_outputs._meta, int):
-        return meta_outputs._meta
-    if isinstance(meta_outputs._meta, torch.SymInt):
-        try:
-            return int(str(meta_outputs._meta))
-        except:
-            pass  # Not static dim size
-    shape = ge.Shape(self, dtype=DataType.DT_INT64)
-    # force unknown shape with ge.Gather when parse symsize
-    return force_op_unknown_shape(ge.Gather(shape, dim))
+    return aten_sym_size_int(self, dim, meta_outputs)
 
 
 @register_fx_node_ge_converter(torch.ops.aten.sym_numel)
