7f7754bdfd92b769f05b5f1154b20443fea509e6
xudaohong <xudaohong@huawei.com>
2025-01-23 07:08:01 +0000

    !1385 [fix] stream tag in torch.ops.aten.expand.default Merge pull request !1385 from xudaohong/stream

--- python/torchair/_ge_concrete_graph/continguous_utils.py
+++ python/torchair/_ge_concrete_graph/continguous_utils.py
@@ -140,8 +140,9 @@ def guard_view_input(func):
             # 判断经过的target是可跳过的view类操作，则刷新反推的meta信息
             # 若为不可跳过的view类操作或计算类节点，则对参数做反推的推导
             # 多流场景，为保证切流后的view算子运行在非默认流上，中断反推逻辑，走算子converter逻辑
-            if is_view_case(target, args, meta_outputs) and (get_default_ge_graph().stream_tag is None):
-                logger.debug(f"Do view optimize, skip the operator {str(target)}")
+            stream_tag = get_default_ge_graph().stream_tag
+            if is_view_case(target, args, meta_outputs) and (stream_tag is None):
+                logger.debug(f"Do view optimize, skip the operator {str(target)}, stream tag: {str(stream_tag)}")
                 ge_outputs = _get_npu_outputs_of_view_ops(target, args, kwargs, meta_outputs)
                 return ge_outputs                     
             else:
--- python/torchair/_ge_concrete_graph/stream_utils.py
+++ python/torchair/_ge_concrete_graph/stream_utils.py
@@ -1,25 +1,27 @@
+from torchair.core.utils import logger
 from torchair.ge._ge_graph import compat_as_bytes, get_default_ge_graph
 from . import ge_apis as ge
 
 
 def set_stream_info(ge_outputs, *args):
     stream_tag = get_default_ge_graph().stream_tag
-    if stream_tag is not None:
-        visited = set()
-        graph = get_default_ge_graph()
-        if isinstance(ge_outputs, ge.Tensor):
-            set_stream_label_and_priority(ge_outputs.node)
-            traverse_input(graph, ge_outputs.node.input, visited, *args)
-        elif isinstance(ge_outputs, (list, tuple)) and all([isinstance(v, ge.Tensor) for v in ge_outputs]):
-            for ge_output in ge_outputs:
-                set_stream_label_and_priority(ge_output.node)
-                traverse_input(graph, ge_output.node.input, visited, *args)
+    if (stream_tag is None) or (ge_outputs is None):
+        return
+    
+    visited = set()
+    graph = get_default_ge_graph()
+    if isinstance(ge_outputs, ge.Tensor):
+        traverse_input(graph, ge_outputs.node, visited, *args)
+    else:
+        for ge_output in ge_outputs:
+            traverse_input(graph, ge_output.node, visited, *args)
 
 
 def set_stream_label_and_priority(node):
     graph = get_default_ge_graph()
     stream_tag = graph.stream_tag
     stream_priority = graph.stream_priority
+    logger.debug(f"set stream_label: {stream_tag}, stream_priority: {stream_priority}, op: {node.name}")
     node.attr["_user_stream_label"].s = compat_as_bytes(stream_tag)
     node.attr["_user_stream_priority"].s = compat_as_bytes(str(stream_priority))
 
@@ -28,7 +30,7 @@ def get_op_from_graph(graph, input_name):
     for op in graph.op:
         if op.name == input_name:
             return op
-    return None
+    raise ValueError(f"can not find {input_name} op in graph {graph.name}")
 
 
 def is_finished(find_op, *args):
@@ -43,27 +45,30 @@ def is_finished(find_op, *args):
     return find_op.type in {'Data', 'Const'}
 
 
-def traverse_input(graph, inputs, visited, *args):
-    for index, input_item in enumerate(inputs):
+def traverse_input(graph, find_op, visited, *args):
+    if is_finished(find_op, *args):
+        return
+    
+    set_stream_label_and_priority(find_op)
+    for input_item in find_op.input:
         if input_item == '':
             continue
-        input_name_list = input_item.split(":")
-        input_name = input_name_list[0]
-        if input_name not in visited:
-            visited.add(input_name)
-            find_op = get_op_from_graph(graph, input_name)
-            if find_op is None:
-                raise ValueError(f"can not find {input_name} op in graph {graph.name}")
-            if not is_finished(find_op, *args):
-                set_stream_label_and_priority(find_op)
-                traverse_input(graph, find_op.input, visited, *args)
-
+        input_name = input_item.split(":")[0]
+        if input_name in visited:
+            continue
+        visited.add(input_name)
+        find_op = get_op_from_graph(graph, input_name)
+        traverse_input(graph, find_op, visited, *args)
+            
 
 def set_stream_tag(stream_tag: str):
+    logger.debug(f"set graph stream tag: {stream_tag}")
     graph = get_default_ge_graph()
     graph.set_stream_tag(stream_tag)
 
 
 def set_stream_priority(stream_priority: int):
+    logger.debug(f"set graph stream priority: {stream_priority}")
     graph = get_default_ge_graph()
     graph.set_stream_priority(stream_priority)
+
