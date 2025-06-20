4bba3a81a532f4cf2574c03be24e3ba3ad52b2b8
鲁博洋 <luboyang@huawei.com>
2025-06-20 09:36:07 +0000

    !1774 aclconfig warnings optimize Merge pull request !1774 from 鲁博洋/warning

--- python/torchair/configs/compiler_config.py
+++ python/torchair/configs/compiler_config.py
@@ -2,6 +2,7 @@
 
 __all__ = ["CompilerConfig"]
 
+import warnings
 from typing import Any
 from torchair.configs._option_base import OptionValue
 from torchair.configs._option_base import DeprecatedValue
@@ -43,41 +44,52 @@ class CompilerConfig(NpuBaseConfig):
         return self_dict == other_dict
 
 
-aclgraph_config_list = [("debug.aclgraph.disable_reinplace_input_mutated_ops_pass", [True]), \
+unsupport_geconfig_list = [("debug.aclgraph.disable_reinplace_input_mutated_ops_pass", [True]), \
     ("debug.aclgraph.disable_reinplace_inplaceable_ops_pass", [True])]
-ge_config_list = [("inference_config.dynamic_gears_merge_policy", ["zip", "product"]), \
+unsupport_aclgraphconfig_list = [("inference_config.dynamic_gears_merge_policy", ["product"]), \
     ("debug.fx_summary.type", ["csv"]), ("dump_config.enable_dump", [True]), \
-    ("ge_config.export_compile_stat", ["2", "1", "0"]), \
+    ("ge_config.export_compile_stat", ["1", "0"]), \
     ("export.experimental.auto_atc_config_generated", [True]), \
     ("export.experimental.enable_record_nn_module_stack", [True]), \
-    ("ge_config.enable_single_stream", [True]), ("ge_config.oo_level", ["O3", "O1"]), \
+    ("ge_config.enable_single_stream", [True]), ("ge_config.oo_level", ["O1"]), \
     ("ge_config.oo_constant_folding", [True, False]), ("ge_config.oo_dead_code_elimination", [True, False]), \
     ("experimental_config.frozen_parameter", [True]), \
-    ("experimental_config.topology_sorting_strategy", ["DFS", "BFS", "RDFS", "StableRDFS"]), \
-    ("experimental_config.cc_parallel_enable", [True]), ("experimental_config.jit_compile", ["auto"]), \
+    ("experimental_config.topology_sorting_strategy", ["BFS", "RDFS", "StableRDFS"]), \
+    ("experimental_config.cc_parallel_enable", [True]), \
     ("experimental_config.enable_ref_data", [True]), \
     ("experimental_config.tiling_schedule_optimize", [True]), \
     ("experimental_config.enable_view_optimize", [False, True]), ("fusion_config.fusion_switch_file", []), \
-    ("experimental_config.static_model_ops_lower_limit", []), ("ge_config.aicore_num", [])]
+    ("experimental_config.static_model_ops_lower_limit", []), ("ge_config.aicore_num", []), \
+    ("ge_config.optimization_switch", [])]
 
 
 def _check_config_support(config: Any):
+    warnings.filterwarnings("once", category=UserWarning)
     config_dict = dict(_get_all_leaf_properties(config))
     warn_config = []
     if config.mode.value == "max-autotune":
-        for config_arg in aclgraph_config_list:
+        for config_arg in unsupport_geconfig_list:
             if config_arg[0] + "._value" in config_dict.keys():
-                warn_config.append("config." + config_arg[0])
-        logger.warning("The following config or properties may not take effect or report error " + \
-            "in max-autotune mode: {warn_configs}".format(warn_configs=", ".join(warn_config)))
+                warn_config = _get_warn_config(warn_config, config_arg, config_dict)
+        warnings.warn("The following torchair config or properties may not take effect or report " + \
+            "error in max-autotune mode: {warn_configs}".format(warn_configs=", ".join(warn_config)), \
+                UserWarning)
     else:
-        for config_arg in ge_config_list:
+        for config_arg in unsupport_aclgraphconfig_list:
             if config_arg[0] + "._value" in config_dict.keys():
-                warn_config.append("config." + config_arg[0])
-        logger.warning("The following config or properties may not take effect or report error " + \
-            "in reduce-overhead mode: {warn_configs}".format(warn_configs=", ".join(warn_config) + \
-            ", cache_compile, set_dim_gears, dynamo_export, scope, npu_print"))
+                warn_config = _get_warn_config(warn_config, config_arg, config_dict)
+        warnings.warn("The following torchair config or properties may not take effect or report " + \
+            "error in reduce-overhead mode: {warn_configs}".format(warn_configs=", ".join(warn_config) + \
+            ", cache_compile, set_dim_gears, dynamo_export, scope, npu_print"), UserWarning)
 
+
+def _get_warn_config(warn_config, config_arg, config_dict):
+    if config_arg[1] == []:
+        warn_config.append("config." + config_arg[0] + ":" + str(config_dict[config_arg[0] + "._value"]))
+    elif config_dict[config_arg[0] + "._value"] in config_arg[1]:
+        warn_config.append("config." + config_arg[0] + ":" + str(config_dict[config_arg[0] + "._value"]))
+    return warn_config
+    
 
 def _get_all_leaf_properties(obj: Any, prefix: str = ""):
     stack = [(prefix, obj)]
