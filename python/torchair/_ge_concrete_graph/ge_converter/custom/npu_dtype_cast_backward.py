23c1996256b3c12824898dfeec592f09c4619670
李根 <ligen75@h-partners.com>
2025-07-08 08:36:47 +0000

    !1834 cast删除多余import Merge pull request !1834 from 李根/master

--- python/torchair/_ge_concrete_graph/ge_converter/custom/npu_dtype_cast_backward.py
+++ python/torchair/_ge_concrete_graph/ge_converter/custom/npu_dtype_cast_backward.py
@@ -8,8 +8,7 @@ from torch import Generator, contiguous_format, inf, strided, SymInt
 from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
 from torchair._ge_concrete_graph import ge_apis as ge
 from torchair._ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
-from torchair.ge._ge_graph import Tensor, TensorSpec, torch_type_to_ge_type, torch_dtype_value_to_ge_type, \
-    torch_dtype_value_to_ge_proto_type, DataType
+from torchair.ge._ge_graph import Tensor, TensorSpec, torch_type_to_ge_type, DataType
 from torchair._ge_concrete_graph.supported_declaration import _TypedTensor, BF16, F32, F16, F64, I32, I16, \
     I64, I8, U8, BOOL, Support
 from torchair._ge_concrete_graph.utils import dtype_promote
