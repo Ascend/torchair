1aad59f3d2909cecea19f91b427c4ce1f8fa42d1
huafeng793 <chenhuafeng2@huawei.com>
2025-11-20 09:13:13 +0800

    【950】conv forward support hifloat8
    Co-authored-by: huafeng793<chenhuafeng2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2138 merge conv_A5 into master
    
    【950】conv forward support hifloat8
    
    Created-by: huafeng793
    Commit-by: huafeng793
    Merged-by: ascend-robot
    Description: <!-- 请根据您的Pull Request类型，保留以下相应的一个模板部分，并删除其他无关部分 -->
    <!-- 请根据您的Pull Request类型，在Pull request标题中标记[Feature]\[Bugfix]\[Refactor] -->
    
    <!-------------------- 新功能模板 BEGIN -------------------->
    ### **【新功能提交】**
    **功能描述**
    quant conv2d接口，torch图模式新增支持hifloat8数据类型
    
    **相关Issue**
    【AR】AR20250604180919
    【lingqu pr】：https://gitee.com/ascend/torchair-for-lingqu2.0/commit/9d3c8889d9d0be39750eb00dff01c5f8427a5f4b
    
    **Checklist**
    - [x] 符合编程规范&&代码红线
    - [x] 具备UT测试用例看护&&测试用例为有效用例（若无测试用例看护请说明原因
    A2A3上无相关UT文件，A5暂不新增
    - [x] 是否涉及对外接口变更
    否
    - [x] 是否涉及官网文档修改
    不涉及文档修改，A5资料暂不开放
    
    <!--------------------  新功能模板 END  -------------------->
    
    See merge request: Ascend/torchair!2138

--- python/torchair/_ge_concrete_graph/ge_converter/custom/quant_conv2d.py
+++ python/torchair/_ge_concrete_graph/ge_converter/custom/quant_conv2d.py
@@ -1,4 +1,18 @@
+import torch
 from torchair._ge_concrete_graph.ge_converter.converter_utils import *
+from torchair.ge._ge_graph import Tensor, TensorSpec, torch_type_to_ge_type, torch_dtype_value_to_ge_type, torch_dtype_value_to_ge_proto_type
+from torchair._ge_concrete_graph.supported_declaration import _TypedTensor, BF16, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, Support
+
+TORCH_DTYPE_MAP = {
+    torch.float16: 5,
+    torch.bfloat16: 15,
+    torch.float32: 6,
+    torch.float8_e5m2: 23,
+    torch.float8_e4m3fn: 24,
+    torch.bits8: 21,
+    torch.int8: 1,
+    torch.int32: 3,
+}
 
 
 @register_fx_node_ge_converter(torch.ops.npu.npu_quant_conv2d.default)
@@ -12,15 +26,18 @@ def conveter_npu_npu_quant_conv2d(
     groups: int = 1,
     offset_x: int = 0,
     round_mode: str = 'rint',
-    output_dtype: torch.dtype = None,
+    output_dtype: int = 0,
     bias: Optional[Tensor] = None,
     offset: Optional[Tensor] = None,
+    x_dtype: int = None,
+    weight_dtype: int = None,
     meta_outputs: TensorSpec = None,
 ):
     """
     NB: aten::npu_quant_conv2d(Tensor input, Tensor weight, Tensor scale, int[2] strides=1,
         int[2] pads=0, int[2] dilations=1, int groups=1, int offset_x=0, str round_mode='rint',
-        ScalarType? output_dtype=None, Tensor? bias=None, Tensor? offset=None) -> Tensor
+        int output_dtype=0, Tensor? bias=None, Tensor? offset=None, int? x_dtype=None,
+        int? weight_dtype=None) -> Tensor
     """
     if meta_outputs.rank != 4:
         raise NotImplementedError("torch.ops.npu.npu_quant_conv2d.default ge converter \
@@ -28,9 +45,17 @@ def conveter_npu_npu_quant_conv2d(
     if offset is not None:
         raise NotImplementedError("torch.ops.npu.npu_quant_conv2d.default ge converter \
                                   is not implement while offset is not None!")
-    if output_dtype != torch.float16:
+    import torch_npu
+    valid_dtypes = {
+        TORCH_DTYPE_MAP[torch.float16],
+        TORCH_DTYPE_MAP[torch.float32],
+        TORCH_DTYPE_MAP[torch.bfloat16],
+        torch_npu.hifloat8
+    }
+    if output_dtype not in valid_dtypes:
         raise NotImplementedError("torch.ops.npu.npu_quant_conv2d.default ge converter \
-                                  is not implement while dtype is not float16!")
+                                  is not implement while output_dtype is not torch.float16 \
+                                  or torch.bfloat16 or torch.float32 or torch_npu.hifloat8!")
     if stride is not None and isinstance(stride, Tensor):
         raise NotImplementedError("torch.ops.npu.npu_quant_conv2d.default ge converter \
                                   is not implement while stride is tensor!")
@@ -44,8 +69,15 @@ def conveter_npu_npu_quant_conv2d(
     strides = [1, 1, stride[0], stride[1]]
     pads = [pads[0], pads[0], pads[1], pads[1]]
     dilations = [1, 1, dilations[0], dilations[1]]
-    ge_output_dtype = torch_type_to_ge_type(output_dtype)
+    ge_output_dtype = torch_dtype_value_to_ge_proto_type(output_dtype)
+    if x_dtype is not None:
+        x.desc.dtype = torch_dtype_value_to_ge_proto_type(x_dtype)
+    if weight_dtype is not None:
+        weight.desc.dtype = torch_dtype_value_to_ge_proto_type(weight_dtype)
 
-    return ge.QuantConv2D(x, weight, scale, bias=bias, offset=None,
-                          dtype=ge_output_dtype, strides=strides, pads=pads, dilations=dilations, groups=groups,
-                          data_format="NCHW", offset_x=offset_x, round_mode=round_mode)
+    out = ge.QuantConv2D(x, weight, scale, bias=bias, offset=None,
+                         dtype=ge_output_dtype, strides=strides, pads=pads, dilations=dilations, groups=groups,
+                         data_format="NCHW", offset_x=offset_x, round_mode=round_mode)
+    if output_dtype == torch_npu.hifloat8:
+        out.desc.dtype = torch_dtype_value_to_ge_proto_type(output_dtype)
+    return out
