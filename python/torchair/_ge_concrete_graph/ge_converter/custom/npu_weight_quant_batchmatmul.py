f494883e164cec95cba2d76b6caf7d22ed6955f2
wangmingli <wangmingli6@h-partners.com>
2025-11-24 11:29:09 +0800

    wqbmm sync v5
    Co-authored-by: wangmingli<wangmingli6@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2150 merge master into master
    
    wqbmm sync v5
    
    Created-by: zhangquanxin
    Commit-by: wangmingli
    Merged-by: ascend-robot
    Description: <!-- 请根据您的Pull Request类型，保留以下相应的一个模板部分，并删除其他无关部分 -->
    <!-- 请根据您的Pull Request类型，在Pull request标题中标记[Feature]\[Bugfix]\[Refactor] -->
    
    <!-------------------- 新功能模板 BEGIN -------------------->
    ### **【新功能提交】**
    
    **功能描述**
    <!-- 请详细描述此次PR所做的更改及其原因 -->
    
    **相关Issue**
    <!-- 如有关联的Issue，请在此处填写，例如: [Fixes issue xxx](链接) -->
    
    **Checklist**
    - [x] 符合编程规范&&代码红线
    - [ ] 具备UT测试用例看护&&测试用例为有效用例（若无测试用例看护请说明原因）
    - [ ] 是否涉及对外接口变更
    - [ ] 是否涉及官网文档修改
    
    <!--------------------  新功能模板 END  -------------------->
    
    <!-------------------- Bug修复模板 BEGIN -------------------->
    ### **【Bug修复】**
    
    **问题描述**
    <!-- 请详细描述此Bug修复PR所针对的问题 -->
    
    **修复方案**
    <!-- 请详细描述此Bug修复PR所做的更改及其原因 -->
    
    **相关Issue**
    <!-- 如有关联的Issue，请在此处填写，例如: [Fixes issue xxx](链接) -->
    
    **Checklist**
    - [x] 符合编程规范&&代码红线
    - [ ] 具备UT测试用例看护&&测试用例为有效用例（若无测试用例看护请说明原因）
    - [ ] 是否涉及对外接口变更
    - [ ] 是否涉及官网文档修改
    <!--------------------  Bug修复模板 END  -------------------->
    
    <!-------------------- Refactor修复模板 BEGIN -------------------->
    ### **【重构/优化】**
    
    **重构/优化描述**
    <!-- 请详细描述此次PR所做的更改及其原因 -->
    
    **Checklist**
    - [x] 符合编程规范&&代码红线
    - [ ] 具备UT测试用例看护&&测试用例为有效用例（若无测试用例看护请说明原因）
    - [ ] 是否涉及对外接口变更
    - [ ] 是否涉及官网文档修改
    <!-------------------- Refactor修复模板 END -------------------->
    
    See merge request: Ascend/torchair!2150

--- python/torchair/_ge_concrete_graph/ge_converter/custom/npu_weight_quant_batchmatmul.py
+++ python/torchair/_ge_concrete_graph/ge_converter/custom/npu_weight_quant_batchmatmul.py
@@ -1,6 +1,30 @@
+from typing import (
+    Any, Callable, ContextManager, Iterator, List, Literal, NamedTuple, Optional, Sequence, Tuple, TypeVar,
+    Union, overload,
+)
+
+import torch
+from torch import Generator, contiguous_format, inf, strided, SymInt
+from torch.types import Device, Number, _bool, _complex, _device, _dtype, _float, _int, _layout, _qscheme, _size
+from torchair._ge_concrete_graph import ge_apis as ge
+from torchair._ge_concrete_graph.fx2ge_converter import declare_supported, register_fx_node_ge_converter
+from torchair.ge._ge_graph import DataType, Tensor, TensorSpec, torch_dtype_value_to_ge_type, \
+    torch_dtype_value_to_ge_proto_type
+from torchair._ge_concrete_graph.supported_declaration import _TypedTensor, F32, F16, F64, I32, I16, I64, I8, U8, BOOL, \
+    Support
+from torchair._ge_concrete_graph.utils import get_cann_opp_version
 from torchair._ge_concrete_graph.ge_converter.converter_utils import *
 
 
+@declare_supported(
+    [
+        # 支持Abf16W8 bf16输出 | Afp16W8 fp16输出
+        # bf16输入时，bias需为fp32 , fp16输入时，bias为fp16
+        Support(F16(32, 11264), F16(11264, 1664), F16(1, 1664), F16(1, 1664)),
+        # 支持Afp16W8, antiquantScale为int64, antiquantOffset为int32，输出为fp16
+        Support(F16(96, 11264), F16(64, 11264), I64(64, 1), I32(64, 1)),
+    ]
+)
 @register_fx_node_ge_converter(torch.ops.npu.npu_weight_quant_batchmatmul.default)
 def conveter_npu_npu_weight_quant_batchmatmul(
     x: Tensor,
@@ -12,6 +36,7 @@ def conveter_npu_npu_weight_quant_batchmatmul(
     bias: Optional[Tensor] = None,
     antiquant_group_size: Optional[int] = 0,
     inner_precise: Optional[int] = 0,
+    weight_dtype: int = None,
     meta_outputs: TensorSpec = None
 ):
     """NB: npu::npu_weight_quant_batchmatmul(Tensor x, Tensor weight, Tensor antiquant_scale,
@@ -37,6 +62,20 @@ def conveter_npu_npu_weight_quant_batchmatmul(
         weight = ge.Reshape(weight, weight_shape)
         if trans_weight:
             weight = ge.Transpose(weight, perm)
+
+    if antiquant_scale.dtype == DataType.DT_UINT8:
+        perm = [1, 0]
+        trans_flag = "Transpose" in antiquant_scale.tensor
+        if trans_flag:
+            antiquant_scale = ge.Transpose(antiquant_scale, perm)
+        antiquant_scale = ge.Bitcast(antiquant_scale, type=DataType.DT_FLOAT8_E8M0)
+        if trans_flag:
+            antiquant_scale = ge.Transpose(antiquant_scale, perm)
+
+    if weight_dtype is not None:
+        weight = ge.Bitcast(weight, type=torch_dtype_value_to_ge_type(weight_dtype))
+        weight.desc.dtype = torch_dtype_value_to_ge_proto_type(weight_dtype)
+
     return ge.WeightQuantBatchMatmulV2(x, weight, antiquant_scale, antiquant_offset=antiquant_offset,
                                        quant_scale=quant_scale, quant_offset=quant_offset, bias=bias,
                                        transpose_x=False, transpose_weight=False,
