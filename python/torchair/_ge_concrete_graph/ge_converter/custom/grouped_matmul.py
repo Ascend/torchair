4f6c3f13059aeb33b6a9662a32281130b1217962
ascend-robot <zhongyuanke@huawei.com>
2025-11-27 10:08:52 +0800

    GMM A5内容合入
    Co-authored-by: liyinghao10<liyinghao10@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2154 merge gmm into master
    
    GMM A5内容合入
    
    Created-by: liyinghao10
    Commit-by: liyinghao10
    Merged-by: ascend-robot
    Description: <!-- 请根据您的Pull Request类型，保留以下相应的一个模板部分，并删除其他无关部分 -->
    <!-- 请根据您的Pull Request类型，在Pull request标题中标记[Feature]\[Bugfix]\[Refactor] -->
    
    <!-------------------- 新功能模板 BEGIN -------------------->
    ### **【新功能提交】**
    
    **功能描述**
    <!-- 请详细描述此次PR所做的更改及其原因 -->
    npu_grouped_matmul A5特性合入
    1) 全量化支持hifloat8/float8类型
    2) 伪量化支持A16F8/A16mxfp4/mxA8W4特性
    
    **相关Issue**
    <!-- 如有关联的Issue，请在此处填写，例如: [Fixes issue xxx](链接) -->
    
    **Checklist**
    - [x] 符合编程规范&&代码红线
    - [ ] 具备UT测试用例看护&&测试用例为有效用例（若无测试用例看护请说明原因）
    - [ ] 是否涉及对外接口变更
    - [ ] 是否涉及官网文档修改
    
    <!--------------------  新功能模板 END  -------------------->
    
    See merge request: Ascend/torchair!2154

--- python/torchair/_ge_concrete_graph/ge_converter/custom/grouped_matmul.py
+++ python/torchair/_ge_concrete_graph/ge_converter/custom/grouped_matmul.py
@@ -1,4 +1,18 @@
 from torchair._ge_concrete_graph.ge_converter.converter_utils import *
+from torchair._utils.check_platform import is_arch35
+from torchair.ge._ge_graph import Tensor, TensorSpec, DataType, torch_dtype_value_to_ge_proto_type, torch_dtype_value_to_ge_type
+
+
+TORCH_DTYPE_MAP = {
+    torch.float16: 5,
+    torch.bfloat16: 15,
+    torch.float32: 6,
+    torch.float8_e5m2: 23,
+    torch.float8_e4m3fn: 24,
+    torch.bits8: 21,
+    torch.int8: 1,
+    torch.int32: 3,
+}
 
 
 def fill_empty_tensorlist(input_data, desired_dtype):
@@ -6,6 +20,28 @@ def fill_empty_tensorlist(input_data, desired_dtype):
         return [ge.Fill([0], ge.Cast(0., dst_type=desired_dtype))]
     else:
         return input_data
+
+
+def fp8_quant_mode(intput_x_dtype, x_dtype, weight_dtype, scale):
+    if scale is None:
+        return False
+    if intput_x_dtype == DataType.DT_FLOAT8_E4M3FN or intput_x_dtype == DataType.DT_FLOAT8_E5M2:
+        return True
+    if (x_dtype is not None and torch_dtype_value_to_ge_type(x_dtype) == DataType.DT_HIFLOAT8) and \
+       (weight_dtype is not None and torch_dtype_value_to_ge_type(weight_dtype) == DataType.DT_HIFLOAT8):
+        return True
+    return False
+
+
+def fp4_quant_mode(x_dtype, weight_dtype, scale):
+    if scale is None:
+        return False
+    if ((x_dtype is not None and torch_dtype_value_to_ge_type(x_dtype) == DataType.DT_FLOAT4_E2M1) and \
+       (weight_dtype is not None and torch_dtype_value_to_ge_type(weight_dtype) == DataType.DT_FLOAT4_E2M1)) or \
+       ((x_dtype is not None and torch_dtype_value_to_ge_type(x_dtype) == DataType.DT_FLOAT4_E1M2) and \
+       (weight_dtype is not None and torch_dtype_value_to_ge_type(weight_dtype) == DataType.DT_FLOAT4_E1M2)):
+        return True
+    return False
 
 
 def convert_tensorlist_to_int4(input_data: List[Tensor]):
@@ -19,7 +55,7 @@ def convert_tensorlist_to_int4(input_data: List[Tensor]):
             if opp_ver >= 8.5:
                 new_w = ge.Bitcast(w_item, type=DataType.DT_INT4, keep_dim=True)
                 w_list.append(new_w)
-            else:                
+            else:
                 const_w = ge.Const([1] * (w_item.rank - 1) + [8])
                 shape_w = ge.Shape(w_item)
                 shape_w = ge.Mul(shape_w, const_w)
@@ -29,6 +65,40 @@ def convert_tensorlist_to_int4(input_data: List[Tensor]):
     else:
         w_list = input_data
     return w_list
+
+
+def convert_tensorlist_to_mxfp4_item(input_data: Tensor, x_dtype, trans):
+    shape_multiples = 2
+    x_ge_dtype = 0
+
+    if x_dtype is not None:
+        x_ge_dtype = torch_dtype_value_to_ge_type(x_dtype)
+    const_x = ge.Const([1] * (input_data.rank - 1) + [shape_multiples])
+    perm = [i for i in range(input_data.rank)]
+    perm[-1], perm[-2] = perm[-2], perm[-1]
+    if trans:
+        input_data = ge.Transpose(input_data, perm)
+    shape_x = ge.Shape(input_data)
+    shape_x = ge.Mul(shape_x, const_x)
+    input_data = ge.Bitcast(input_data, type=x_ge_dtype)
+    input_data = ge.Reshape(input_data, shape_x)
+    if trans:
+        input_data = ge.Transpose(input_data, perm)
+    return input_data
+
+
+def convert_tensorlist_to_mxfp4(x: List[Tensor], weight: List[Tensor], x_dtype, weight_dtype):
+    x_list = []
+    w_list = []
+    for x_item in x:
+        new_x = convert_tensorlist_to_mxfp4_item(x_item, x_dtype, False)
+        x_list.append(new_x)
+    for w_item in weight:
+        # x只有一个x_item
+        trans_weight = x_item.symsize[-1] == w_item.symsize[-2]
+        new_w = convert_tensorlist_to_mxfp4_item(w_item, weight_dtype, trans_weight)
+        w_list.append(new_w)
+    return x_list, w_list
 
 
 def convert_scale_tensorlist(scales: List[Tensor]):
@@ -45,13 +115,17 @@ def convert_scale_tensorlist(scales: List[Tensor]):
 
 def convert_ydtype_in_a8(output_dtype: Optional[int] = None):
     y_dtype = -1
-    if output_dtype == torch.float16:
+    if output_dtype == TORCH_DTYPE_MAP[torch.float16]:
         y_dtype = 0
-    elif output_dtype == torch.bfloat16:
+    elif output_dtype == TORCH_DTYPE_MAP[torch.bfloat16]:
         y_dtype = 1
-    elif output_dtype == torch.int32:
+    elif output_dtype == TORCH_DTYPE_MAP[torch.int32]:
         y_dtype = 2
-    elif output_dtype == torch.int8:
+    elif output_dtype == TORCH_DTYPE_MAP[torch.float32]:
+        if not is_arch35():
+            raise ValueError("output_dtype does not support float32 in versions prior to A5.")
+        y_dtype = 3
+    elif output_dtype == TORCH_DTYPE_MAP[torch.int8]:
         raise ValueError("output_dtype not support int8 yet for graph mode")
     else:
         raise ValueError(f"output_dtype should be float16, bfloat16 or int32, "
@@ -61,9 +135,9 @@ def convert_ydtype_in_a8(output_dtype: Optional[int] = None):
 
 def convert_ydtype_in_a4(output_dtype: Optional[int] = None):
     y_dtype = -1
-    if output_dtype == torch.float16:
+    if output_dtype == TORCH_DTYPE_MAP[torch.float16]:
         y_dtype = 0
-    elif output_dtype == torch.bfloat16:
+    elif output_dtype == TORCH_DTYPE_MAP[torch.bfloat16]:
         y_dtype = 1
     else:
         raise ValueError(f"output_dtype should be float16, bfloat16, "
@@ -91,50 +165,110 @@ def conveter_npu_npu_grouped_matmul(
     act_type: Optional[int] = 0,
     tuning_config: Optional[List[int]] = None,
     output_dtype: Optional[int] = None,
+    x_dtype: Optional[int] = None,
+    weight_dtype: Optional[int] = None,
+    scale_dtype: Optional[int] = None,
+    per_token_scale_dtype: Optional[int] = None,
     meta_outputs: TensorSpec = None,
 ):
-    """NB: npu::npu_grouped_matmul(x, weight, *, bias=None, scale=None, offset=None, antiquant_scale=None, 
+    """NB: npu::npu_grouped_matmul(x, weight, *, bias=None, scale=None, offset=None, antiquant_scale=None,
             antiquant_offset=None, per_token_scale=None, group_list=None,
-            activation_input=None, activation_quant_offset=None, activation_quant_offset=None, 
+            activation_input=None, activation_quant_offset=None, activation_quant_offset=None,
             split_item=0, group_type=-1, group_list_type=0, act_type=0,
-            tuning_config=[0], output_dtype=None) -> Tensor[]
+            tuning_config=[0], output_dtype=None, x_dtype=None, weight_dtype=None, scale_dtype=None, per_token_scale_dtype=None) -> Tensor[]
     """
-    tuning_config = tuning_config or [0]
-    x_dtype = x[0].dtype
 
-    if x_dtype == DataType.DT_BF16:
-        bias = fill_empty_tensorlist(bias, DataType.DT_FLOAT)
-    elif x_dtype == DataType.DT_INT8:
-        bias = fill_empty_tensorlist(bias, DataType.DT_INT32)
+    import torch_npu
+
+    tuning_config = tuning_config or [0]
+    input_x_dtype = x[0].dtype
+    w_dtype = weight[0].dtype
+
+    if fp8_quant_mode(input_x_dtype, x_dtype, weight_dtype, scale):
+        if bias is None:
+            bias = fill_empty_tensorlist(bias, DataType.DT_FLOAT)
+    elif fp4_quant_mode(x_dtype, weight_dtype, scale):
+        if bias is None:
+            bias = fill_empty_tensorlist(bias, DataType.DT_FLOAT)
     else:
-        bias = fill_empty_tensorlist(bias, x_dtype)
+        if input_x_dtype == DataType.DT_BF16:
+            bias = fill_empty_tensorlist(bias, DataType.DT_FLOAT)
+        elif input_x_dtype == DataType.DT_INT8:
+            if w_dtype == DataType.DT_INT32:
+                bias = fill_empty_tensorlist(bias, DataType.DT_FLOAT)
+            else:
+                bias = fill_empty_tensorlist(bias, DataType.DT_INT32)
+        elif input_x_dtype == DataType.DT_FLOAT8_E4M3FN and w_dtype == DataType.DT_FLOAT:
+            ge_out_type = torch_dtype_value_to_ge_type(output_dtype)
+            bias = fill_empty_tensorlist(bias, ge_out_type)
+        else:
+            bias = fill_empty_tensorlist(bias, input_x_dtype)
 
     scale = fill_empty_tensorlist(scale, DataType.DT_UINT64)
     scale = convert_scale_tensorlist(scale)
     offset = fill_empty_tensorlist(offset, DataType.DT_FLOAT)
 
-    w_dtype = weight[0].dtype
-
-    if (x_dtype == DataType.DT_FLOAT16 or x_dtype == DataType.DT_BF16) and w_dtype == DataType.DT_INT8:
-        antiquant_scale = fill_empty_tensorlist(antiquant_scale, x_dtype)
-        antiquant_offset = fill_empty_tensorlist(antiquant_offset, x_dtype)
+    if (input_x_dtype == DataType.DT_FLOAT16 or input_x_dtype == DataType.DT_BF16) and (w_dtype == DataType.DT_INT8 or
+                                                                                        w_dtype == DataType.DT_FLOAT):
+        antiquant_scale = fill_empty_tensorlist(antiquant_scale, input_x_dtype)
+        antiquant_offset = fill_empty_tensorlist(antiquant_offset, input_x_dtype)
+    elif input_x_dtype == DataType.DT_FLOAT8_E4M3FN and w_dtype == DataType.DT_FLOAT:
+        ge_out_type = torch_dtype_value_to_ge_type(output_dtype)
+        antiquant_scale = fill_empty_tensorlist(antiquant_scale, ge_out_type)
+        antiquant_offset = fill_empty_tensorlist(antiquant_offset, ge_out_type)
     else:
         antiquant_scale = fill_empty_tensorlist(antiquant_scale, DataType.DT_FLOAT16)
         antiquant_offset = fill_empty_tensorlist(antiquant_offset, DataType.DT_FLOAT16)
 
+    if antiquant_scale[0].dtype == DataType.DT_UINT8:
+        antiquant_scale[0] = ge.Bitcast(antiquant_scale[0], type=DataType.DT_FLOAT8_E8M0)
+        antiquant_scale[0].desc.dtype = DataType.DT_FLOAT8_E8M0
+
     y_dtype = -1
-    if output_dtype is not None and x[0].dtype == DataType.DT_INT8:
-        y_dtype = convert_ydtype_in_a8(output_dtype)
-    elif output_dtype is not None and x[0].dtype == DataType.DT_INT32:
-        y_dtype = convert_ydtype_in_a4(output_dtype)
+    if is_arch35():
+        if output_dtype is not None:
+            y_dtype = convert_ydtype_in_a8(output_dtype)
+    else:
+        if output_dtype is not None and x[0].dtype == DataType.DT_INT8:
+            y_dtype = convert_ydtype_in_a8(output_dtype)
+        elif output_dtype is not None and x[0].dtype == DataType.DT_INT32:
+            y_dtype = convert_ydtype_in_a4(output_dtype)
 
     per_token_scale = per_token_scale[0] if per_token_scale is not None and len(per_token_scale) else None
 
     if group_list is not None and not isinstance(group_list, Tensor):
         group_list = dtype_promote(group_list, target_dtype=torch.int64)
 
-    w_list = convert_tensorlist_to_int4(weight)
-    x_list = convert_tensorlist_to_int4(x)
+    x_list = []
+    w_list = []
+    if weight[0].dtype == DataType.DT_INT32 and not is_arch35():
+        x_list = x
+        w_list = convert_tensorlist_to_int4(weight)
+    elif x_dtype is not None and weight_dtype is not None and \
+         (x_dtype == torch_npu.float4_e2m1fn_x2 or x_dtype == torch_npu.float4_e1m2fn_x2) and \
+         (weight_dtype == torch_npu.float4_e2m1fn_x2 or weight_dtype == torch_npu.float4_e1m2fn_x2):
+        x_list, w_list = convert_tensorlist_to_mxfp4(x, weight, x_dtype, weight_dtype)
+    else:
+        x_list = x
+        w_list = weight
+
+    if x_dtype is not None:
+        if x_dtype != torch_npu.float4_e2m1fn_x2 and x_dtype != torch_npu.float4_e1m2fn_x2:
+            x_list[0] = ge.Bitcast(x_list[0], type=torch_dtype_value_to_ge_type(x_dtype))
+        x_list[0].desc.dtype = torch_dtype_value_to_ge_proto_type(x_dtype)
+
+    if weight_dtype is not None:
+        if weight_dtype != torch_npu.float4_e2m1fn_x2 and weight_dtype != torch_npu.float4_e1m2fn_x2:
+            w_list[0] = ge.Bitcast(w_list[0], type=torch_dtype_value_to_ge_type(weight_dtype))
+        w_list[0].desc.dtype = torch_dtype_value_to_ge_proto_type(weight_dtype)
+
+    if scale_dtype is not None:
+        scale[0] = ge.Bitcast(scale[0], type=torch_dtype_value_to_ge_type(scale_dtype))
+        scale[0].desc.dtype = torch_dtype_value_to_ge_proto_type(scale_dtype)
+
+    if per_token_scale_dtype is not None:
+        per_token_scale = ge.Bitcast(per_token_scale, type=torch_dtype_value_to_ge_type(per_token_scale_dtype))
+        per_token_scale.desc.dtype = torch_dtype_value_to_ge_proto_type(per_token_scale_dtype)
 
     return ge.GroupedMatmul(x_list, w_list, bias, scale, offset, antiquant_scale, antiquant_offset, group_list,
                             per_token_scale, split_item=split_item, dtype=y_dtype, transpose_weight=False,
