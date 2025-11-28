e23e42228253b4f9eb62682eb849565453347976
mihudan <wangkai579@huawei.com>
2025-11-28 17:28:45 +0800

    debug dump 日志信息优化
    Co-authored-by: wangkai<wangkai579@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2246 merge dump_safty into master
    
    debug dump 日志信息优化
    
    Created-by: mihudan
    Commit-by: wangkai
    Merged-by: ascend-robot
    Description: 问题描述
    debug dump日志信息修改  
    1 落盘无路径日志提醒
    2 无需每次执行pass前后打印fx图，只打印用户自定义pass
    3 _gm和gm混淆导致打印的日志一直为优化的fx图
    
    修复方案
    1 增加落盘路径日志
    2 只保留用户自定义pass前后的fx图的日志打印
    3. 更新变量名称，_gm为gm， pass_count应修改为file_count
    
    非新需求，无需添加ST
    
    
    See merge request: Ascend/torchair!2246

--- python/torchair/_utils/graph_transform_observer.py
+++ python/torchair/_utils/graph_transform_observer.py
@@ -15,32 +15,35 @@ class GraphTransformObserver:
     '''
     Custom FX Pass observer to print debug information before and after FX Pass execution.
     '''
-    __pass_count = 0
+    __file_count = 0
 
     def __init__(self, gm: GraphModule, example_inputs, config) -> None:
-        self._gm = gm
+        self.gm = gm
         self.example_inputs = example_inputs
         self.config = config
         self._phase = getattr(debug_ctx, 'phase', 'default')
         self._path = os.path.join(DebugContext.current_path(), f"{self._phase}")
-        self.dump_enabled = os.getenv("TORCH_COMPILE_DEBUG", "0") == "1"
+        self._dump_enabled = os.getenv("TORCH_COMPILE_DEBUG", "0") == "1"
         self._logger = logger
        
     @classmethod
-    def get_current_pass_count(cls):
-        return cls.__pass_count
+    def get_current_file_count(cls):
+        return cls.__file_count
 
     @classmethod
-    def reset_pass_count(cls):
-        cls.__pass_count = 0
+    def reset_file_count(cls):
+        cls.__file_count = 0
     
-    def apply_gm_pass(self, pass_fn: Callable, pass_name: str):
-        self._logger.debug('PASS_%s [%s] before [%s] execution, graph is %s',
-                            GraphTransformObserver.__pass_count, self._phase, pass_name, self._gm.graph)
+    def apply_gm_pass(self, pass_fn: Callable, pass_name: str, enable_log=False):
+        if enable_log:
+            self._logger.debug('[%s] before [%s] execution, graph is %s', self._phase, pass_name, self.gm.graph)
+            
         res = pass_fn(self.gm, self.example_inputs, self.config)
-        self._logger.debug('PASS_%s [%s] after [%s] execution, graph is %s', 
-                           GraphTransformObserver.__pass_count, self._phase, pass_name, self._gm.graph)
-        if not self.dump_enabled:
+
+        if enable_log:
+            self._logger.debug('[%s] after [%s] execution, graph is %s', self._phase, pass_name, self.gm.graph)
+            
+        if not self._dump_enabled:
             return res
         self.dump_gm(self.gm, "graph_after_" + pass_name)
         return res
@@ -52,7 +55,7 @@ class GraphTransformObserver:
             apply_gegraph_pass(self, pass_fn, example_inputs, config):
         """
         res = pass_fn(ge)
-        if not self.dump_enabled:
+        if not self._dump_enabled:
             return res
         self.dump_gegraph(ge, "graph_after_" + pass_name)
         return res
@@ -61,27 +64,27 @@ class GraphTransformObserver:
         """
         Non-pass function that requires sequential invocation with an ordinal number.
         """
-        if not self.dump_enabled:
+        if not self._dump_enabled:
             return
         os.makedirs(self._path, exist_ok=True)
         file_type = self.config.debug.graph_dump.type.value or "pbtxt"
         file_name = (
-        f"{GraphTransformObserver.__pass_count:03d}_aot_"
+        f"{GraphTransformObserver.__file_count:03d}_aot_"
         f"{self._phase}_{file_name}.{file_type}"
         )   
         dump_graph(os.path.join(self._path, file_name), ge)
-        GraphTransformObserver.__pass_count += 1
+        GraphTransformObserver.__file_count += 1
         return
 
     def dump_gm(self, gm: GraphModule, file_name: str):
         """
         Non-pass function that requires sequential invocation with an ordinal number.
         """
-        if not self.dump_enabled:
+        if not self._dump_enabled:
             return
         dump_fx_safety(gm, os.path.join(
-            self._path, f"{GraphTransformObserver.__pass_count:03d}_aot_{self._phase}_{file_name}.txt"))
-        GraphTransformObserver.__pass_count += 1
+            self._path, f"{GraphTransformObserver.__file_count:03d}_aot_{self._phase}_{file_name}.txt"))
+        GraphTransformObserver.__file_count += 1
         return
 
 
@@ -98,7 +101,13 @@ class DebugContext():
         return self
     
     def __exit__(self, exc_type, exc_val, exc_tb):
-        GraphTransformObserver.reset_pass_count()
+        GraphTransformObserver.reset_file_count()
+        if os.getenv("TORCH_COMPILE_DEBUG", "0") == "1":
+            logger.debug(
+                "[%s] Full debug info saved to: %s",
+                getattr(debug_ctx, 'phase', 'default'),
+                self.current_path()
+            )
         if exc_type is None:
             return True
         else:
