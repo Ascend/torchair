d89eaa817ebc25139c4481b624a2f6ae5b533d64
rich9527 <zhouruiqi5@huawei.com>
2026-02-03 21:24:18 +0800

    Log error if AttributeError occurs during custom converter import
    Co-authored-by: 周锐淇<zhouruiqi5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2599 merge master into master
    
    Log error if AttributeError occurs during custom converter import
    
    Created-by: rich9527
    Commit-by: 周锐淇
    Merged-by: ascend-robot
    Description: Provide an error log if an AttributeError is raised while importing a custom converter.
    
    See merge request: Ascend/torchair!2599

--- python/torchair/_ge_concrete_graph/ge_converter/custom/__init__.py
+++ python/torchair/_ge_concrete_graph/ge_converter/custom/__init__.py
@@ -1,6 +1,7 @@
 import os
 import pkgutil
 import importlib
+import warnings
 
 
 __all__ = list(module for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]))
@@ -9,5 +10,5 @@ for name in __all__:
     module_name = "torchair._ge_concrete_graph.ge_converter.custom." + name
     try:
         importlib.import_module(module_name)
-    except AttributeError:
-        pass
+    except AttributeError as e:
+        warnings.warn(f"Failed to import {module_name} due to AttributeError: {e}")
