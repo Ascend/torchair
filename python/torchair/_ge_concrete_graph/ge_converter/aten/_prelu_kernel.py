1e8ef5e36fb37e0d27e3ceb2cbb4ef72d6180380
transformer910 <tianye82@h-partners.com>
2026-02-25 16:53:38 +0800

    新增 prelu, prelu_kernel_backward 接口的图模式
    Co-authored-by: transformer910<tianye82@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2642 merge br_0225 into master
    
    新增 prelu, prelu_kernel_backward 接口的图模式
    
    Created-by: transformer910
    Commit-by: transformer910
    Merged-by: ascend-robot
    Description: torchair support support prelu, prelu_kernel_backward
    
    See merge request: Ascend/torchair!2642

--- python/torchair/_ge_concrete_graph/ge_converter/aten/_prelu_kernel.py
+++ python/torchair/_ge_concrete_graph/ge_converter/aten/_prelu_kernel.py
@@ -1,9 +1,14 @@
 from torchair._ge_concrete_graph.ge_converter.converter_utils import *
 
 
+@declare_supported(
+    [
+        Support([F32(2, 3, 4), F32(3)]),
+    ]
+)
 @register_fx_node_ge_converter(torch.ops.aten._prelu_kernel.default)
 def conveter_aten__prelu_kernel_default(
     self: Tensor, weight: Tensor, meta_outputs: TensorSpec = None
 ):
     """NB: aten::_prelu_kernel(Tensor self, Tensor weight) -> Tensor"""
-    raise NotImplementedError("torch.ops.aten._prelu_kernel.default ge_converter is not implemented!")
+    return ge.PRelu(self, weight)
--- python/torchair/_ge_concrete_graph/ge_converter/aten/_prelu_kernel_backward.py
+++ python/torchair/_ge_concrete_graph/ge_converter/aten/_prelu_kernel_backward.py
@@ -1,9 +1,14 @@
 from torchair._ge_concrete_graph.ge_converter.converter_utils import *
 
 
+@declare_supported(
+    [
+        Support([F32(2, 3, 4), F32(2, 3, 4), F32(3)]),
+    ]
+)
 @register_fx_node_ge_converter(torch.ops.aten._prelu_kernel_backward.default)
 def conveter_aten__prelu_kernel_backward_default(
     grad_output: Tensor, self: Tensor, weight: Tensor, meta_outputs: TensorSpec = None
 ):
     """NB: aten::_prelu_kernel_backward(Tensor grad_output, Tensor self, Tensor weight) -> (Tensor, Tensor)"""
-    raise NotImplementedError("torch.ops.aten._prelu_kernel_backward.default ge_converter is not implemented!")
+    return ge.PReluGrad(grad_output, self, weight)
