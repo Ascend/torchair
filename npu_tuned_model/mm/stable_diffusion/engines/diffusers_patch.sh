cedb5b2054423eecc2192e5aa274bc1e890fef8b
Sunyp01 <sunyaping4@huawei.com>
2026-02-27 17:38:51 +0800

    remove redundant models
    Co-authored-by: SunYaping<sunyaping4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2628 merge master into master
    
    remove redundant models
    
    Created-by: Sunyp01
    Commit-by: SunYaping
    Merged-by: ascend-robot
    Description: remove redundant models
    
    See merge request: Ascend/torchair!2628

--- npu_tuned_model/mm/stable_diffusion/engines/diffusers_patch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# coding=utf-8
-
-diffusers_path=$(pip3 show diffusers | grep Location | awk '{print $2}')
-cp diffusers_npu.patch ${diffusers_path}/diffusers
-cd ${diffusers_path}/diffusers
-
-patch -p4 < diffusers_npu.patch
