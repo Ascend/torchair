6ac3f2fe48e8d131a5cda5f9c2361d73203e680b
libohao6 <libohao3@huawei.com>
2026-03-06 11:10:33 +0800

    修复alltoallvgmm静态图问题
    Co-authored-by: libohao6<libohao3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2695 merge master_0305_fix into master
    
    修复alltoallvgmm静态图问题
    
    Created-by: libohao6
    Commit-by: libohao6
    Merged-by: ascend-robot
    Description: 修复alltoallvgmm静态图问题
    
    See merge request: Ascend/torchair!2695

--- python/torchair/_ge_concrete_graph/ge_converter/custom/allto_allv_grouped_matmul.py
+++ python/torchair/_ge_concrete_graph/ge_converter/custom/allto_allv_grouped_matmul.py
@@ -19,17 +19,53 @@ def convert_npu_alltoallv_gmm(
     permute_out_flag: bool = False,
     meta_outputs: TensorSpec = None
 ):
-    return ge.AlltoAllvGroupedMatMul(
+    gmm_x_scale = None
+    gmm_weight_scale = None
+    gmm_x_offset = None
+    gmm_weight_offset = None
+    mm_x_scale = None
+    mm_weight_scale = None
+    mm_x_offset = None
+    mm_weight_offset = None
+    gmm_x_quant_mode = 0
+    gmm_weight_quant_mode = 0
+    mm_x_quant_mode = 0
+    mm_weight_quant_mode = 0
+    group_size = 0
+    y_dtype = 28
+    mm_dtype = 28
+    dependencies = []
+    node_name = None
+
+    gmm_y, mm_y, permute_out = ge.AlltoAllvGroupedMatMul(
         gmm_x=gmm_x,
         gmm_weight=gmm_weight,
         send_counts_tensor=send_counts_tensor,
         recv_counts_tensor=recv_counts_tensor,
         mm_x=mm_x,
         mm_weight=mm_weight,
+        gmm_x_scale=gmm_x_scale,
+        gmm_weight_scale=gmm_weight_scale,
+        gmm_x_offset=gmm_x_offset,
+        gmm_weight_offset=gmm_weight_offset,
+        mm_x_scale=mm_x_scale,
+        mm_weight_scale=mm_weight_scale,
+        mm_x_offset=mm_x_offset,
+        mm_weight_offset=mm_weight_offset,
         group=hcom,
         ep_world_size=ep_world_size,
         send_counts=send_counts,
         recv_counts=recv_counts,
         trans_gmm_weight=trans_gmm_weight,
         trans_mm_weight=trans_mm_weight,
-        permute_out_flag=permute_out_flag)
+        permute_out_flag=permute_out_flag,
+        gmm_x_quant_mode=gmm_x_quant_mode,
+        gmm_weight_quant_mode=gmm_weight_quant_mode,
+        mm_x_quant_mode=mm_x_quant_mode,
+        mm_weight_quant_mode=mm_weight_quant_mode,
+        group_size=group_size,
+        y_dtype=y_dtype,
+        mm_dtype=mm_dtype,
+        dependencies=dependencies,
+        node_name=node_name)
+    return gmm_y, mm_y, permute_out
