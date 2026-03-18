28d1a3ee900f1fbb5a169bbb81bd172423c197a5
huangyuxiaaaaa <huangyuxia9@huawei.com>
2026-03-18 14:54:59 +0800

    新增LogSigmoid算子torch图配置
    Co-authored-by: huangyuxiaaaaa<huangyuxia9@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2751 merge LogSigmoid into master
    
    新增LogSigmoid算子torch图配置
    
    Created-by: huangyuxiaaaaa
    Commit-by: huangyuxiaaaaa
    Merged-by: ascend-robot
    Description: 关联issue：
    https://gitcode.com/Ascend/torchair/issues/379
    
    新增LogSigmoid算子torch图配置
    
    See merge request: Ascend/torchair!2751

--- python/torchair/_ge_concrete_graph/ge_converter/aten/log_sigmoid_forward.py
+++ python/torchair/_ge_concrete_graph/ge_converter/aten/log_sigmoid_forward.py
@@ -4,7 +4,9 @@ from torchair._ge_concrete_graph.ge_converter.converter_utils import *
 @register_fx_node_ge_converter(torch.ops.aten.log_sigmoid_forward.default)
 def conveter_aten_log_sigmoid_forward_default(self: Tensor, meta_outputs: TensorSpec = None):
     """NB: aten::log_sigmoid_forward(Tensor self) -> (Tensor output, Tensor buffer)"""
-    raise NotImplementedError("torch.ops.aten.log_sigmoid_forward.default ge_converter is not implemented!")
+    result = ge.LogSigmoid(self)
+    buffer = ge.Identity(self)
+    return result, buffer
 
 
 @register_fx_node_ge_converter(torch.ops.aten.log_sigmoid_forward.output)
@@ -16,4 +18,6 @@ def conveter_aten_log_sigmoid_forward_output(
     meta_outputs: TensorSpec = None
 ):
     """NB: aten::log_sigmoid_forward.output(Tensor self, *, Tensor(a!) output, Tensor(b!) buffer) -> (Tensor(a!), Tensor(b!))"""
-    raise NotImplementedError("torch.ops.aten.log_sigmoid_forward.output ge_converter is not implemented!")
+    res = ge.LogSigmoid(self)
+    buf = ge.Identity(self)
+    return res, buf
