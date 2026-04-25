4a16e5565f582c067b56c009c8bf62122719d3fd
lhlll <luohao148@huawei.com>
2026-04-25 15:41:34 +0800

    LI A5修改入参
    Co-authored-by: 我是石头人大王<luohao148@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2986 merge master into master
    
    LI A5修改入参
    
    Created-by: lhlll
    Commit-by: 我是石头人大王
    Merged-by: ascend-robot
    Description: LI A5修改入参
    
    See merge request: Ascend/torchair!2986

--- python/torchair/_ge_concrete_graph/ge_converter/custom/lightning_indexer.py
+++ python/torchair/_ge_concrete_graph/ge_converter/custom/lightning_indexer.py
@@ -30,7 +30,7 @@ def convert_npu_lightning_indexer(
     sparse_mode: int = 3,
     pre_tokens: int = 9223372036854775807,
     next_tokens: int = 9223372036854775807,
-    return_values: bool = False,
+    return_value: bool = False,
     meta_outputs: TensorSpec = None,
 ):
     return torchair.ge.custom_op(
@@ -48,7 +48,7 @@ def convert_npu_lightning_indexer(
                "sparse_mode": attr.Int(sparse_mode),
                "pre_tokens": attr.Int(pre_tokens),
                "next_tokens": attr.Int(next_tokens),
-               "return_values": attr.Bool(return_values),
+               "return_values": attr.Bool(return_value),
                },
         outputs=["sparse_indices",
                  "sparse_values",
