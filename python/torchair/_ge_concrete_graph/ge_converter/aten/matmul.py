0a49a9ec375aa2f08908c595b2c5d7130b0864a7
hulida <hulida@huawei.com>
2026-05-25 14:19:32 +0800

    issue481 torch2.10下，converter支持输入多维，matmul转化为ge.batchmatmul
    Co-authored-by: hulida<hulida@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3078 merge master into master
    
    issue481 torch2.10下，converter支持输入多维，matmul转化为ge.batchmatmul
    
    Created-by: hulida
    Commit-by: hulida
    Merged-by: ascend-robot
    Description: issue481 torch2.10下，converter支持输入多维情况下，matmul转化为ge.batchmatmul
    
    
    https://gitcode.com/Ascend/torchair/issues/481
    
    torch2.10的aot没有把多维tensor的matmul转成batchmatmul，导致ge IR上会拿多维tensor做matmul报错，现ge matmul converter增加多维matmul转化为ge.batchmatmul逻辑
    
    See merge request: Ascend/torchair!3078

--- python/torchair/_ge_concrete_graph/ge_converter/aten/matmul.py
+++ python/torchair/_ge_concrete_graph/ge_converter/aten/matmul.py
@@ -1,11 +1,21 @@
 from torchair._ge_concrete_graph.ge_converter.converter_utils import *
 
 
+@declare_supported(
+    [
+        Support(F32(2, 3), F32(3, 4)),
+        Support(F32(10, 2, 3), F32(10, 3, 4)),
+        Support(F16(3, 2, 2), F16(2, 2)),
+    ]
+)
 @register_fx_node_ge_converter(torch.ops.aten.matmul.default)
 def conveter_aten_matmul_default(self: Tensor, other: Tensor, meta_outputs: TensorSpec = None):
     """NB: aten::matmul(Tensor self, Tensor other) -> Tensor"""
     if self.dtype == DataType.DT_INT8 or other.dtype == DataType.DT_INT8:
         raise RuntimeError("torch.ops.aten.matmul.default ge_converter is not support int8 dtype!")
+    if self.rank >= 3 or other.rank >= 3:
+        self, other = dtype_promote(self, other, target_dtype=meta_outputs.dtype)
+        return ge.BatchMatMul(self, other)
     return ge.MatMul(self, other, None)
 
 
