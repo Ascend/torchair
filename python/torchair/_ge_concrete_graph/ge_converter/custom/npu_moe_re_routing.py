17bdfb16ca29e95640654b25384276c7531220e4
wuxiyuan <wuxiyuan@huawei.com>
2026-05-26 14:53:19 +0800

    [fix]修复MoeReRouting算子FP4场景下错误解包问题
    Co-authored-by: wuxiyuan<wuxiyuan@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3077 merge master into master
    
    [fix]修复MoeReRouting算子FP4场景下错误解包问题
    
    Created-by: wuxiyuan
    Commit-by: wuxiyuan
    Merged-by: ascend-robot
    Description: [fix]修复MoeReRouting算子FP4场景下错误解包问题
    
    https://gitcode.com/Ascend/torchair/issues/483
    
    See merge request: Ascend/torchair!3077

--- python/torchair/_ge_concrete_graph/ge_converter/custom/npu_moe_re_routing.py
+++ python/torchair/_ge_concrete_graph/ge_converter/custom/npu_moe_re_routing.py
@@ -1,5 +1,45 @@
-from torchair._ge_concrete_graph.ge_converter.converter_utils import *
-from torchair.ge._ge_graph import torch_dtype_value_to_ge_proto_type
+from torchair.ge._ge_graph import (
+    Tensor,
+    TensorSpec,
+    DataType,
+    torch_dtype_value_to_ge_type,
+    torch_dtype_value_to_ge_proto_type,
+)
+from torchair._ge_concrete_graph.ge_converter.converter_utils import (
+    ge,
+    torch,
+    register_fx_node_ge_converter,
+    Optional,
+    List,
+)
+
+
+def unpack_uint8_to_float4(tokens: Tensor, tokens_dtype: int) -> Tensor:
+    target_dtype = torch_dtype_value_to_ge_type(tokens_dtype)
+    tokens_const = ge.Const([1] * (tokens.rank - 1) + [2])
+    tokens_shape = ge.Shape(tokens)
+    tokens_new_shape = ge.Mul(tokens_shape, tokens_const)
+    tokens = ge.Bitcast(tokens, type=target_dtype)
+    tokens.desc.dtype = torch_dtype_value_to_ge_proto_type(tokens_dtype)
+    tokens = ge.Reshape(tokens, tokens_new_shape)
+    return tokens
+
+
+def pack_int4_to_uint8(tensor: Tensor) -> Tensor:
+    bit_shape = [1, 2]
+    div_x2 = ge.Cast(ge.Const(bit_shape), dst_type=DataType.DT_INT32)
+    y_shape_int4 = ge.Shape(tensor)
+    y_shape_uint8 = ge.Div(y_shape_int4, div_x2)
+    y_shape_int4_2bit = ge.ConcatV2(
+        [y_shape_uint8, ge.Cast(ge.Const([2]), dst_type=DataType.DT_INT32)], concat_dim=0, N=2
+    )
+    y = ge.Bitcast(ge.Reshape(tensor, y_shape_int4_2bit), type=DataType.DT_UINT8)
+    permute_tokens = ge.Reshape(y, y_shape_uint8)
+    return permute_tokens
+
+
+def is_fp4_dtype(dtype: int) -> bool:
+    return torch_dtype_value_to_ge_type(dtype) in (DataType.DT_FLOAT4_E2M1, DataType.DT_FLOAT4_E1M2)
 
 
 @register_fx_node_ge_converter(torch.ops.npu.npu_moe_re_routing.default)
@@ -13,14 +53,21 @@ def conveter_npu_moe_re_routing_default(
         tokens_dtype: Optional[int] = None,
         meta_outputs: List[TensorSpec] = None):
     if tokens_dtype is not None:
-        tokens.desc.dtype = torch_dtype_value_to_ge_proto_type(tokens_dtype)
-    
+        if tokens.dtype == DataType.DT_UINT8 and is_fp4_dtype(tokens_dtype):
+            tokens = unpack_uint8_to_float4(tokens, tokens_dtype)
+        else:
+            tokens = ge.Bitcast(tokens, type=torch_dtype_value_to_ge_type(tokens_dtype))
+            tokens.desc.dtype = torch_dtype_value_to_ge_proto_type(tokens_dtype)
+
     permute_tokens, permute_per_token_scales, permute_token_idx, expert_token_num = \
         ge.MoeReRouting(tokens, expert_token_num_per_rank, per_token_scales,
                         expert_token_num_type=expert_token_num_type,
                         idx_type=idx_type)
-    
+
     if tokens_dtype is not None:
-        permute_tokens.desc.dtype = torch_dtype_value_to_ge_proto_type(tokens_dtype)
-    
+        if is_fp4_dtype(tokens_dtype):
+            permute_tokens = pack_int4_to_uint8(permute_tokens)
+        else:
+            permute_tokens.desc.dtype = torch_dtype_value_to_ge_proto_type(tokens_dtype)
+
     return permute_tokens, permute_per_token_scales, permute_token_idx, expert_token_num
